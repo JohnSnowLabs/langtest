53501a1e7760a330f58a7c65c096cb7259bea212
Ali Tarık <tarik@johnsnowlabs.com>
2023-02-01 11:27:54 +0300

    nlptest.py docstring fixes

--- nlptest_redesign/nlptest/nlptest.py
+++ nlptest_redesign/nlptest/nlptest.py
@@ -28,7 +28,7 @@ class Harness:
 
         Args:
             task (str, optional): Task for which the model is to be evaluated.
-            model (str | ModelFactory): Model to be evaluated.
+            model (str | ModelFactory): ModelFactory object or path to the model to be evaluated.
             data (str, optional): Path to the data to be used for evaluation.
             config (str | dict, optional): Configuration for the tests to be performed.
         """
