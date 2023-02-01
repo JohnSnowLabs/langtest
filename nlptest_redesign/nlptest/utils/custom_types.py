30b6dee2fc93e276d5623f8b451df28a97a8d327
JulesBelveze <jules.belveze@hotmail.fr>
2023-02-01 18:22:22 +0100

    chore: WIP NEROutput entity normalization

--- nlptest_redesign/nlptest/utils/custom_types.py
+++ nlptest_redesign/nlptest/utils/custom_types.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 
 
 class NEROutput(BaseModel):
@@ -7,6 +7,29 @@ class NEROutput(BaseModel):
     start: int
     end: int
     score: float = None
+
+    @validator("entity")
+    def normalize_entity(cls, entity):
+        """"""
+        mapper = {
+            "PERSON": "PER",  # english spacy models
+            "PS": "PER",  # korean spacy models
+            "LC": "LOC",  # korean spacy models
+            "OG": "ORG",  # korean spacy models
+            "DT": "DATE",  # korean spacy models
+            "TI": "TIME",  # korean spacy models
+            "QT": "QUANTITY",  # korean spacy models,
+            "EVN": "EVENT",
+            "MSR": "",
+            "OBJ": "PRODUCT",
+            "PRS": "PER",
+            "TME": "TIME",
+            "WRK": "WORK_OF_ART"
+        }
+        for k, v in mapper.items():
+            if k in entity:
+                return entity.replace(k, v)
+        return entity
 
     class Config:
         extra = "ignore"
