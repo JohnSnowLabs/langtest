28fcf72f79c1c78423c01eef67d242551bfe34d6
Kalyan Chakravarthy <chakravarthik27@gmail.com>
2023-02-05 20:57:24 +0530

    docstrings add in __init__.py

--- nlptest_redesign/nlptest/__init__.py
+++ nlptest_redesign/nlptest/__init__.py
@@ -3,6 +3,28 @@ from nlptest.datahandler.datasource import DataFactory
 from nlptest.modelhandler.modelhandler import ModelFactory
 from nlptest.transform.pertubation import PertubationFactory
 
+"""
+nlptest is python library package which can useful for testing of nlp models
+Like Spacy, HuggingFace, SparkNLP ...etc
+
+To testing of NLP Models by import this library as follows
+
+Harness is a class, which can be instaniting and do testing in flow like
+workflow: generate --> run --> report --> save 
+>>> from nlptest import Harness 
+
+ModelFactory is for handling of strings to models 
+like access or download resources from cloud.
+>>> from nlptest import ModelFactory
+
+DataFactory is for handling of like csv, json, conll ...
+>>> from nlptest import DataFactory
+
+Augumentation of NLP datasets like Perturbations(
+uppercase, lowercase, add_context, add_contractions) 
+>>> from nlptest import PerturbationFactory
+
+"""
 __all__ = [
     Harness,
     ModelFactory,
