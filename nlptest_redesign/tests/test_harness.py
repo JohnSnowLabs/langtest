ac8fac59a45d10bf7bbeb0928d195b8f8b579ad7
Kalyan Chakravarthy <chakravarthik27@gmail.com>
2023-02-06 12:10:46 +0530

    test_attributes added

--- nlptest_redesign/tests/test_harness.py
+++ nlptest_redesign/tests/test_harness.py
@@ -4,6 +4,8 @@ import pandas as pd
 sys.path.insert(0,"..")
 
 from nlptest.nlptest import Harness
+from nlptest.datahandler.datasource import DataFactory
+from nlptest.modelhandler.modelhandler import ModelFactory
 
 
 class HarnessTestCase(unittest.TestCase):
@@ -19,6 +21,17 @@ class HarnessTestCase(unittest.TestCase):
 
     def test_Harness(self):
         self.assertIsInstance(self.harness, Harness)
+
+
+    def test_attributes(self):
+        """
+        Testing Attributes of Harness Class
+        """
+        self.assertIsInstance(self.harness.task, str) 
+        self.assertIsInstance(self.harness.model, (str, ModelFactory)) 
+        # self.assertIsInstance(self.harness.data, (str, DataFactory)) 
+        self.assertIsInstance(self.harness._config, (str, dict)) 
+
     
     def test_generate_testcases(self):
         df = self.harness.generate() #._load_testcases
