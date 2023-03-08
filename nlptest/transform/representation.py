a49835abf012fb5bf23081fc2d0bf357c48f89d4
Kalyan Chakravarthy <chakravarthik27@gmail.com>
2023-03-08 15:25:27 +0530

    fix error

--- nlptest/transform/representation.py
+++ nlptest/transform/representation.py
@@ -7,10 +7,6 @@ from nlptest.utils.custom_types import Sample
 
 
 class BaseRepresentation(ABC):
-<<<<<<< HEAD
-
-=======
->>>>>>> e4be3befcb71af4921610a51797e81c520be6edd
     @staticmethod
     @abstractmethod
     def transform(self):
