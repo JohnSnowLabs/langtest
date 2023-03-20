1e13137787859a0a8e43d5ad81e691f9f1668a88
Ali Tarik <alimirik55@gmail.com>
2023-03-20 13:17:35 +0300

    remove warning

--- nlptest/utils/gender_classifier.py
+++ nlptest/utils/gender_classifier.py
@@ -1,9 +1,10 @@
 import torch
+import logging
 from transformers import pipeline, AutoModelForSequenceClassification, AutoTokenizer
-
 
 class GenderClassifier():
     def __init__(self) -> None:
+        logging.getLogger("transformers").setLevel(logging.ERROR)
         tokenizer = AutoTokenizer.from_pretrained("microsoft/xtremedistil-l6-h256-uncased")
         model = AutoModelForSequenceClassification.from_pretrained("microsoft/xtremedistil-l6-h256-uncased", num_labels=3)
         ckpts = torch.load("checkpoints.ckpt")
