d202a4116e1c024f78a5730e6a6bffc42c0fef36
Kalyan Chakravarthy <chakravarthik27@gmail.com>
2023-03-24 17:39:48 +0530

    Testing SparkNLP

--- tests/test_sparknlp_model.py
+++ tests/test_sparknlp_model.py
@@ -1,32 +1,32 @@
-# import os
-# import unittest
+import os
+import unittest
 
-# from nlptest import Harness
-# from nlptest.modelhandler import ModelFactory
+from nlptest import Harness
+from nlptest.modelhandler import ModelFactory
 
 
-# class SparkNLPTestCase(unittest.TestCase):
+class SparkNLPTestCase(unittest.TestCase):
 
-#     def setUp(self) -> None:
-#         self.params = {
-#             "task": 'ner',
-#             "model": "ner.dl",
-#             "data": "demo/data/test.conll",
-#             "config": "demo/data/config.yml",
-#             "hub": "johnsnowlabs"
-#         }
+    def setUp(self) -> None:
+        self.params = {
+            "task": 'ner',
+            "model": "ner.dl",
+            "data": "demo/data/test.conll",
+            "config": "demo/data/config.yml",
+            "hub": "johnsnowlabs"
+        }
 
-#     def test_predict(self):
-#         """
-#         Testing Instance after in Harness Class
-#         """
-#         harness = Harness(**self.params)
-#         self.assertIsInstance(harness, Harness)
+    def test_predict(self):
+        """
+        Testing Instance after in Harness Class
+        """
+        harness = Harness(**self.params)
+        self.assertIsInstance(harness, Harness)
 
-#     def test_outputCol(self):
-#         """
-#         Testing Attributes of Harness Class
-#         """
-#         harness = Harness(**self.params)
-#         self.assertIsInstance(harness.model, (str, ModelFactory))
-#         self.assertIsInstance(harness.model.model_class.output_col, str)
+    def test_outputCol(self):
+        """
+        Testing Attributes of Harness Class
+        """
+        harness = Harness(**self.params)
+        self.assertIsInstance(harness.model, (str, ModelFactory))
+        self.assertIsInstance(harness.model.model_class.output_col, str)
