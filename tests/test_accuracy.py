82bc119cf64da5e51c49c08d2828a9074f0765ba
JulesBelveze <jules.belveze@hotmail.fr>
2023-03-30 15:09:24 +0200

    fix: test_accuracy

--- tests/test_accuracy.py
+++ tests/test_accuracy.py
@@ -2,18 +2,29 @@ import unittest
 
 from nlptest import Harness
 
+
 class AccuracyTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.h_spacy = Harness("ner", model="en_core_web_sm", data="demo/data/test.conll")
+        self.h_spacy = Harness("ner", model="en_core_web_sm", data="demo/data/test.conll", hub="spacy")
         self.h_spacy.configure({
             'tasks': ['ner'],
-            'tests_types': ['lowercase'],
-            'min_pass_rate':{'default':0.5}
+            'defaults': {
+                'min_pass_rate': 0.65
+            },
+            'tests':
+                {
+                    "robustness": {
+                        "uppercase": {
+                            "min_pass_rate": 0.70
+                        }
+                    }
+                }
         })
-        self.h_spacy.generate().run().report()
+        self.h_spacy.generate().run()
 
     def test_accuracy_report(self) -> None:
-        acc_report = self.h_spacy.accuracy_report()
+        """"""
+        acc_report = self.h_spacy.report()
         self.assertGreaterEqual(acc_report.shape[0], 1)
-        self.assertEqual(acc_report.shape[1], 5)
+        self.assertEqual(acc_report.shape[1], 7)
