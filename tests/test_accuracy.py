8db9b98577ea5abefc05491e19e6f68b8a6e27a7
luca-martial <lucamrtl@gmail.com>
2023-04-03 09:40:49 +0200

    Revert "Revert "Refacto: codebase clean up""
    This reverts commit 91c501ff

--- tests/test_accuracy.py
+++ tests/test_accuracy.py
@@ -9,11 +9,18 @@ class AccuracyTestCase(unittest.TestCase):
         self.h_spacy = Harness("ner", model="en_core_web_sm",
                                data="demo/data/test.conll", hub="spacy")
         self.h_spacy.configure(
-            {'defaults': {'min_pass_rate': 0.65, 'min_representation': 50},
-             'tests': {'robustness': {'lowercase': {'min_pass_rate': 0.65}
-                                      }
-                       }
-             })
+            {'defaults': {
+                'min_pass_rate': 0.65,
+                'min_representation': 50
+            },
+             'tests': {
+                 'robustness': {
+                     'lowercase': {
+                         'min_pass_rate': 0.65
+                     }
+                 }
+             }
+            })
         self.h_spacy.generate().run().report()
 
     def test_accuracy_report(self) -> None:
