f1519bdf8e90b38bb7b54071276599d907c9b5ec
Ali Tarik <alimirik55@gmail.com>
2023-04-06 12:29:42 +0300

    support latest python version

--- setup.py
+++ setup.py
@@ -132,7 +132,7 @@ setup(
     # 'Programming Language' classifiers above, 'pip install' will check this
     # and refuse to install the project if the version does not match. See
     # https://packaging.python.org/guides/distributing-packages-using-setuptools/#python-requires
-    python_requires=">=3.7, <=3.11",
+    python_requires=">=3.7, <3.12",
     # This field lists other packages that your project depends on to run.
     # Any package you put here will be installed by pip when your project is
     # installed, so they must be valid existing projects.
