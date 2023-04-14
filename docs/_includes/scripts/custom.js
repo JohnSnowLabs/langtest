cbd382f2d85bd3dc407e7fcceab598af5ee4dc9b
Lev <agsfer@gmail.com>
2023-04-14 13:33:19 +0300

    Update custom.js

--- docs/_includes/scripts/custom.js
+++ docs/_includes/scripts/custom.js
@@ -73,5 +73,4 @@ try {
           
   }
 tabs('.tabs-wrapper', '.tabs-header', '.tab-btn', '.tabs-item', 'active');
-//tabs('#one_liner_text_tab', '.tabs-header', '.tab-btn', '.tabs-item', 'active');
 } catch(e){}
