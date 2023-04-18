6b83b15bbb21918dcdf6696d8706cf22de9d3825
luca-martial <lucamrtl@gmail.com>
2023-04-18 18:02:36 +0200

    add sphinx docs to website

--- sphinx/move_folders_for_building_website.sh
+++ sphinx/move_folders_for_building_website.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
 echo "Move _static"
-grep -RiIl '_static' _build | xargs sed -i 's/_static/static/g'
+grep -RiIl '_static' _build | xargs sed -i '' 's/_static/static/g'
 mv _build/html/_static _build/html/static
 
-echo "Move _modules"
-grep -RiIl '_modules' _build | xargs sed -i 's/_modules/modules/g'
-mv _build/html/_modules _build/html/modules
-
-echo "Move references/_autosummary"
-grep -RiIl '_autosummary' _build | xargs sed -i 's/_autosummary/autosummary/g'
-mv _build/html/reference/_autosummary _build/html/reference/autosummary
+echo "Move _autosummary"
+grep -RiIl '_autosummary' _build | xargs sed -i '' 's/_autosummary/autosummary/g'
+mv _build/html/_autosummary _build/html/autosummary
 
 rm -rf _build/html/_sources
