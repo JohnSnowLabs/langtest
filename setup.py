b18412a5e2bc800bb847ffb6bcdd324ba6b806be
Arshaan <arshaan666@gmail.com>
2023-05-02 12:44:07 +0530

    update conda meta yaml

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/guides/single-sourcing-package-version/
-    version="1.0.2",  # Required
+    version="1.1.0",  # Required
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
