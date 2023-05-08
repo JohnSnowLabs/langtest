4bd16b1678b29eda79fb540168395e72ba336606
Kalyan Chakravarthy <chakravarthik27@gmail.com>
2023-05-08 18:58:21 +0530

    Implemented ConfigError class to handle errors

--- nlptest/modelhandler/llm_modelhandler.py
+++ nlptest/modelhandler/llm_modelhandler.py
@@ -2,9 +2,11 @@ import inspect
 from typing import Union
 import langchain.llms as lc
 from langchain import LLMChain, PromptTemplate
+from pydantic import ValidationError
 from ..modelhandler.modelhandler import _ModelHandler
 
 DEFAULT_LLM_HUB = {hub.lower(): hub for hub in lc.__all__}
+
 
 class PretrainedModelForQA(_ModelHandler):
 
@@ -30,6 +32,12 @@ class PretrainedModelForQA(_ModelHandler):
             raise ValueError(
                 f'''Model "{path}" is not found online or local.
                 Please install langchain by pip install langchain''')
+        except ValidationError as e:
+            error_msg = [err['loc'][0] for err in e.errors()]
+
+            raise ConfigError(
+                f"\nplease update model_parameters section in config.yml file for {path} model in {hub}.\nmodel_parameters:\n\t{error_msg[0]}: value \n\n{error_msg} is required field(s), please provide them in config.yml "
+            )
 
     def predict(self, text: Union[str, dict], prompt: dict, *args, **kwargs):
         prompt_template = PromptTemplate(**prompt)
@@ -39,23 +47,13 @@ class PretrainedModelForQA(_ModelHandler):
     def __call__(self, text: Union[str, dict], prompt: dict, *args, **kwargs):
         """Alias of the 'predict' method"""
         return self.predict(text, prompt, *args, **kwargs)
-    
-    def default_args(self, parameter: str, hub: str):
 
-        defaults_args = {
-            'ai21': {
-                'model_name': 'model',
-                'max_tokens': 'maxToken'
-            },
-            'anthropic': {
-                'model_name': 'model',
-                'max_tokens': 'max_tokens_to_sample'
-            },
-            'cohere': {
-                'model_name': 'model',
-                'max_tokens': 'max_tokens_to_sample'
-            }
 
-        }
-        return defaults_args[hub][parameter]
+class ConfigError(BaseException):
 
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return self.message
