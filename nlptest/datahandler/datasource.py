25192d7c1b72f7b6e9bce710f4f0ed9268d8b737
Ali Tarik <alimirik55@gmail.com>
2023-05-11 00:34:32 +0300

    refactor to handle nq open

--- nlptest/datahandler/datasource.py
+++ nlptest/datahandler/datasource.py
@@ -442,11 +442,14 @@ class JSONLDataset(_IDataset):
         data = []
         with jsonlines.open(self._file_path) as reader:
             for item in reader:
+                expected_results = item.get("answer_and_def_correct_predictions", item.get("answer", None))
+                if isinstance(expected_results, str) or isinstance(expected_results, bool): expected_results = [str(expected_results)]
+
                 data.append(
                     QASample(
                         original_question = item['question'],
                         original_context= item.get('passage', "-"),
-                        expected_results = item.get('answer', None),
+                        expected_results = expected_results,
                         task=self.task,
                         dataset_name=self._file_path.split('/')[-2]
                         )
