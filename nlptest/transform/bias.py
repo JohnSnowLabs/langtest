2f459288ed9a4ac8dc44a4704f46972852c4bde9
Kalyan Chakravarthy <chakravarthik27@gmail.com>
2023-05-19 16:26:01 +0530

    update bias for ner task

--- nlptest/transform/bias.py
+++ nlptest/transform/bias.py
@@ -150,7 +150,7 @@ class GenderPronounBias(BaseBias):
                     span = re.search(regex, replaced_string)
                     replaced_string = re.sub(
                         regex, chosen_token, replaced_string, count=1)
-                    if not "task" in sample.__annotations__:
+                    if sample.task == "ner":
                         transformations.append(
                             Transformation(
                                 original_span=Span(
@@ -207,7 +207,7 @@ class CountryEconomicBias(BaseBias):
                     span = re.search(regex, replaced_string)
                     replaced_string = re.sub(
                         regex, chosen_token, replaced_string, count=1)
-                    if not "task" in sample.__annotations__:
+                    if sample.task in "ner":
                         transformations.append(
                             Transformation(
                                 original_span=Span(
@@ -271,7 +271,7 @@ class EthnicityNameBias(BaseBias):
                     span = re.search(regex, replaced_string)
                     replaced_string = re.sub(
                         regex, chosen_token, replaced_string, count=1)
-                    if not "task" in sample.__annotations__:
+                    if sample.task == "ner":
                         transformations.append(
                             Transformation(
                                 original_span=Span(
@@ -330,7 +330,7 @@ class ReligionBias(BaseBias):
                     span = re.search(regex, replaced_string)
                     replaced_string = re.sub(
                         regex, chosen_token, replaced_string, count=1)
-                    if not "task" in sample.__annotations__:
+                    if sample.task == "ner":
                         transformations.append(
                             Transformation(
                                 original_span=Span(
