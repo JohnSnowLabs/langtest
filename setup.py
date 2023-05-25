f7eb00e3f0f5be848570f7a1870463fc7e759555
Arshaan <arshaan666@gmail.com>
2023-05-25 15:53:48 +0530

    updated setup and meta.yaml

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/guides/single-sourcing-package-version/
-    version="1.3.0.rc.1",  # Required
+    version="1.3.0",  # Required
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
