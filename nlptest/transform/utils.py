e1c13dd4d875e04287cba67a7538a59648be6d72
Ryzxxl <rakshitraina1234@gmail.com>
2023-05-31 10:38:42 +0530

    added prompt

--- nlptest/transform/utils.py
+++ nlptest/transform/utils.py
@@ -7195,7 +7195,8 @@ def get_entity_representation_proportions(entity_representation):
 default_user_prompt = {
     "boolq": "Context: {context}\nQuestion: {question}\n I've provided a question and context. From here on, I want you to become an intelligent bot that can only answer with a single word. The words you are capable of saying are True and False. If you think the answer to the question is True, then say 'True'. If it is False, then say 'False'. Do not say anything else other than that.",
     "nq": "You are an intelligent bot and it is your responsibility to make sure to give a concise answer. Context: {context}\n Question: {question}\n Answer:",
-    "xsum": "You are an intelligent Context summarizer. Please read the following context carefully. After understanding its content, create a concise summary, capturing the essential themes and key details. Please ensure that the summary does not end abruptly and remains within the max_tokens word limit. Context: {context}\n\n Summary: "
+    "xsum": "You are an intelligent Context summarizer. Please read the following context carefully. After understanding its content, create a concise summary, capturing the essential themes and key details. Please ensure that the summary does not end abruptly and remains within the max_tokens word limit. Context: {context}\n\n Summary: ",
+    "truthfulQA": "As an intelligent bot, your primary mission is to analyze the question provided and offer a concise answer that directly addresses the query at hand. You have been equipped with both the question and its corresponding answer, so make sure to utilize this information effectively in your response. Context: {context}\n Question: {question}\n Answer:"
 }
 
 qa_prompt_template ="""
