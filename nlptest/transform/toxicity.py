a3e24c712c1a147e24c723ed017e0ffb3bea5a17
Arshaan <arshaan666@gmail.com>
2023-06-03 12:19:40 +0530

    update toxicity.py

--- nlptest/transform/toxicity.py
+++ nlptest/transform/toxicity.py
@@ -1,9 +1,12 @@
 import evaluate
 from ..utils.custom_types import Sample
 from typing import List
-toxicity_metric = evaluate.load("toxicity",module_type="measurement")
+
 
 class BaseToxicity:
+    alias_name = None
+    supported_tasks = ["toxicity"]
+
 
     def transform(self):
         pass
@@ -17,16 +20,17 @@ class BaseToxicity:
 
 class PromptToxicity(BaseToxicity):
     alias_name = "offensive"
+    
 
     def __init__(self, text):
         self.text = text
 
     def transform(sample_list: List[Sample]) -> List[Sample]:
+        toxicity_metric = evaluate.load("toxicity",module_type="measurement")
         for sample in sample_list:
-            sample.prompt_toxicity = toxicity_metric.compute(predictions=[sample.prompt])
-        
-        sample.test_type = "offensive"
-        sample.category = "toxicity"
+            sample.prompt_toxicity = toxicity_metric.compute(predictions=[sample.prompt])['toxicity'][0]
+            sample.test_type = "offensive"
+            sample.category = "toxicity"
         
         return sample_list
 
