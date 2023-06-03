64d20a0bab3f85154403743fe64a8190efab3c97
Kalyan Chakravarthy <chakravarthik27@gmail.com>
2023-06-03 16:50:45 +0530

    run method update

--- nlptest/transform/toxicity.py
+++ nlptest/transform/toxicity.py
@@ -1,7 +1,11 @@
+import asyncio
 import evaluate
 from abc import ABC, abstractmethod
 from ..utils.custom_types import Sample
 from typing import List
+
+
+toxicity_metric = evaluate.load("toxicity", module_type="measurement")
 
 
 class BaseToxicity(ABC):
@@ -10,36 +14,47 @@ class BaseToxicity(ABC):
 
     @staticmethod
     @abstractmethod
-    def transform(self):
+    def transform(sample_list: List[Sample]):
         return NotImplementedError()
 
     @staticmethod
     @abstractmethod
-    async def run(self):
-        return NotImplementedError()
+    async def run(sample_list: List[Sample], model, *args, **kwargs):
+        progress = kwargs.get("progress", False)
+        for sample in sample_list:
+            if sample.state != "done":
+                if hasattr(sample, "run"):
+                    sample_status = sample.run(model, *args, **kwargs)
+                    if sample_status:
+                        sample.state = "done"
+                else:
+                    sample.completion = model(sample.prompt)
+                    global toxicity_metric
+                    sample.completion_toxicity = toxicity_metric.compute(
+                        predictions=[sample.completion])['toxicity'][0]
+                    sample.state = "done"
+
+            if progress:
+                progress.update(1)
+        return sample_list
 
     @classmethod
-    async def async_run(cls):
-        return NotImplementedError()
-
+    async def async_run(cls, sample_list: List[Sample], model, *args, **kwargs):
+        created_task = asyncio.create_task(
+            cls.run(sample_list, model, **kwargs)
+        )
+        return created_task
 
 
 class PromptToxicity(BaseToxicity):
     alias_name = "offensive"
-    
 
     def transform(sample_list: List[Sample]) -> List[Sample]:
-        toxicity_metric = evaluate.load("toxicity",module_type="measurement")
         for sample in sample_list:
-            sample.prompt_toxicity = toxicity_metric.compute(predictions=[sample.prompt])['toxicity'][0]
+            global toxicity_metric
+            sample.prompt_toxicity = toxicity_metric.compute(
+                predictions=[sample.prompt])['toxicity'][0]
             sample.test_type = "offensive"
             sample.category = "toxicity"
-        
+
         return sample_list
-
-    async def async_run(self, sample_list: List[Sample], model, *args, **kwargs):
-
-        return self.transform()
-
-    def run(self):
-        return self.transform()
