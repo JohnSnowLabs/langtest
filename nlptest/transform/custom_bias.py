1f53c21c0f9fdbabfc9f3192060fd92cf4b6f610
Ryzxxl <rakshitraina1234@gmail.com>
2023-06-09 14:27:39 +0530

    restructue custom_bias.py

--- nlptest/transform/custom_bias.py
+++ nlptest/transform/custom_bias.py
@@ -1,60 +1,33 @@
-from .utils import (asian_names, black_names, country_economic_dict, hispanic_names, inter_racial_names, male_pronouns,
-                    native_american_names, neutral_pronouns, religion_wise_names, white_names,female_pronouns)
-
-
-
-
+from .utils import (
+    asian_names, black_names, country_economic_dict, hispanic_names, inter_racial_names, male_pronouns,
+    native_american_names, neutral_pronouns, religion_wise_names, white_names, female_pronouns
+)
+from .bias import *
 
 def add_custom_data(file, name):
-    custom_data = file
-    new_name = name
-
-    if new_name in (
-            "replace_to_high_income_country",
-            "replace_to_low_income_country",
-            "replace_to_upper_middle_income_country",
-            "replace_to_lower_middle_income_country"
-    ):
+    if name in CountryEconomicBias.alias_name:
         valid_names = country_economic_dict.keys()
+
         # Validate the schema
-        if not set(custom_data.keys()).issubset(valid_names):
+        if not set(file.keys()).issubset(valid_names):
             raise ValueError(f"Invalid schema. It should be one of: {', '.join(valid_names)}.")
 
         # Append values to existing keys
-        for key, values in custom_data.items():
+        for key, values in file.items():
             country_economic_dict[key] += values
 
-    elif new_name in (
-            "replace_to_muslim_names",
-            "replace_to_hindu_names",
-            "replace_to_christian_names",
-            "replace_to_sikh_names",
-            "replace_to_jain_names",
-            "replace_to_parsi_names",
-            "replace_to_buddhist_names"
-    ):
+    elif name in ReligionBias.alias_name:
         valid_names = religion_wise_names.keys()
 
         # Validate the schema
-        if not set(custom_data.keys()).issubset(valid_names):
+        if not set(file.keys()).issubset(valid_names):
             raise ValueError(f"Invalid schema. It should be one of: {', '.join(valid_names)}.")
 
         # Append values to existing keys
-        for key, values in custom_data.items():
+        for key, values in file.items():
             religion_wise_names[key] += values
 
-    elif new_name in (
-            "replace_to_white_firstnames",
-            "replace_to_black_firstnames",
-            "replace_to_hispanic_firstnames",
-            "replace_to_asian_firstnames",
-            "replace_to_white_lastnames",
-            "replace_to_black_lastnames",
-            "replace_to_hispanic_lastnames",
-            "replace_to_asian_lastnames",
-            "replace_to_native_american_lastnames",
-            "replace_to_inter_racial_lastnames"
-    ):
+    elif name in EthnicityNameBias.alias_name:
         valid_names = (
             'white_names',
             'black_names',
@@ -64,7 +37,7 @@ def add_custom_data(file, name):
             'inter_racial_names'
         )
 
-        for data_dict in custom_data:
+        for data_dict in file:
             if 'name' not in data_dict:
                 raise ValueError("Invalid JSON format. 'name' key is missing.")
 
@@ -83,7 +56,7 @@ def add_custom_data(file, name):
 
             if set(data_dict.keys()) - {'name', 'first_names', 'last_names'}:
                 raise ValueError(f"Invalid keys in the JSON for '{name}'. "
-                                    f"Only the following keys are allowed: 'name', 'first_names', 'last_names'.")
+                                 f"Only the following keys are allowed: 'name', 'first_names', 'last_names'.")
 
             if name == 'white_names':
                 white_names['first_names'] += first_names
@@ -102,22 +75,17 @@ def add_custom_data(file, name):
             elif name == 'inter_racial_names':
                 inter_racial_names['last_names'] += last_names
 
-    elif new_name in (
-            "replace_to_male_pronouns",
-            "replace_to_female_pronouns",
-            "replace_to_neutral_pronouns"
-    ):
+    elif name in GenderPronounBias.alias_name:
         valid_names = ('female_pronouns', 'male_pronouns', 'neutral_pronouns')
 
-        for data_dict in custom_data:
+        for data_dict in file:
             if 'name' not in data_dict:
                 raise ValueError("Invalid JSON format. 'name' key is missing.")
 
             name = data_dict['name']
 
             if name not in valid_names:
-                raise ValueError(
-                    f"Invalid 'name' value '{name}'. It should be one of: {', '.join(valid_names)}.")
+                raise ValueError(f"Invalid 'name' value '{name}'. It should be one of: {', '.join(valid_names)}.")
 
             pronouns = {
                 'subjective_pronouns': data_dict.get('subjective_pronouns', []),
@@ -127,13 +95,13 @@ def add_custom_data(file, name):
             }
 
             if all(key not in pronouns for key in
-                    ['subjective_pronouns', 'objective_pronouns', 'reflexive_pronouns', 'possessive_pronouns']):
+                   ['subjective_pronouns', 'objective_pronouns', 'reflexive_pronouns', 'possessive_pronouns']):
                 raise ValueError(
                     f"Missing pronoun keys in the JSON for '{name}'. Please include at least one of: "
                     "'subjective_pronouns', 'objective_pronouns', 'reflexive_pronouns', 'possessive_pronouns'.")
 
             invalid_keys = set(data_dict.keys()) - {'name', 'subjective_pronouns', 'objective_pronouns',
-                                                        'reflexive_pronouns', 'possessive_pronouns'}
+                                                     'reflexive_pronouns', 'possessive_pronouns'}
             if invalid_keys:
                 raise ValueError(
                     f"Invalid keys in the JSON for '{name}': {', '.join(invalid_keys)}. "
@@ -155,4 +123,3 @@ def add_custom_data(file, name):
                 neutral_pronouns['objective_pronouns'] += pronouns['objective_pronouns']
                 neutral_pronouns['reflexive_pronouns'] += pronouns['reflexive_pronouns']
                 neutral_pronouns['possessive_pronouns'] += pronouns['possessive_pronouns']
-
