67b379a322e5f607deffb3f15b08402f81dfe7e2
Arshaan <arshaan666@gmail.com>
2023-07-10 23:14:08 +0530

    fix linting

--- langtest/transform/robustness.py
+++ langtest/transform/robustness.py
@@ -506,16 +506,25 @@ class SwapEntities(BaseRobustness):
 
                 replace_token = sent_tokens[replace_idx : replace_idx + len(replace_idxs)]
                 replace_token = " ".join(replace_token)
-                filtered_ents = [ent for ent in terminology[ent_type] if len(ent.split(" ")) == token_length]
-                if not filtered_ents: 
+                filtered_ents = [
+                    ent
+                    for ent in terminology[ent_type]
+                    if len(ent.split(" ")) == token_length
+                ]
+                if not filtered_ents:
                     chosen_ent = replace_token
                 else:
                     chosen_ent = random.choice(filtered_ents)
 
                 if random.random() < prob:
-                    replace_token_pos = re.search(re.escape(replace_token), sample.original)
+                    replace_token_pos = re.search(
+                        re.escape(replace_token), sample.original
+                    )
                     sample.test_case = sample.original.replace(replace_token, chosen_ent)
-                    if sample.task in ("ner", "text-classification") and replace_token_pos:
+                    if (
+                        sample.task in ("ner", "text-classification")
+                        and replace_token_pos
+                    ):
                         sample.transformations = [
                             Transformation(
                                 original_span=Span(
