2f1db05d048fa773f03f267ad8d5fe9d30b09cee
Prikshit7766 <prikshitsharma8024@gmail.com>
2023-07-20 12:17:21 +0530

    updated langtest.py for custom-column-names for csv

--- langtest/langtest.py
+++ langtest/langtest.py
@@ -11,7 +11,7 @@ from pkg_resources import resource_filename
 
 from langtest.utils.custom_types.sample import RuntimeSample
 from .augmentation import AugmentRobustness, TemplaticAugment
-from .datahandler.datasource import DataFactory, HuggingFaceDataset
+from .datahandler.datasource import DataFactory, HuggingFaceDataset,CustomCSVDataset
 from .modelhandler import LANGCHAIN_HUBS, ModelFactory
 from .transform import TestFactory
 from .transform.utils import RepresentationOperation
@@ -145,35 +145,60 @@ class Harness:
             and hub in self.SUPPORTED_HUBS_HF_DATASET_CLASSIFICATION
             and task == "text-classification"
         ):
-            self.data = (
-                HuggingFaceDataset(data["name"], task=task).load_data(
-                    feature_column=data.get("feature_column", "text"),
-                    target_column=data.get("target_column", "label"),
-                    split=data.get("split", "test"),
-                    subset=data.get("subset", None),
-                )
-                if data is not None
-                else None
-            )
-
-            if hub == "spacy" and (model == "textcat_imdb" or model is None):
-                if model is None:
-                    logging.warning(
-                        "Using the default 'textcat_imdb' model for Spacy hub. Please provide a custom model path if desired."
+            if not data["name"].endswith(".csv"):
+                self.data = (
+                    
+                    HuggingFaceDataset(data["name"], task=task).load_data(
+                        feature_column=data.get("feature_column", "text"),
+                        target_column=data.get("target_column", "label"),
+                        split=data.get("split", "test"),
+                        subset=data.get("subset", None),
                     )
-                model = resource_filename("langtest", "data/textcat_imdb")
+                    if data is not None
+                    else None
+                )
+
+                if hub == "spacy" and (model == "textcat_imdb" or model is None):
+                    if model is None:
+                        logging.warning(
+                            "Using the default 'textcat_imdb' model for Spacy hub. Please provide a custom model path if desired."
+                        )
+                    model = resource_filename("langtest", "data/textcat_imdb")
+            elif data["name"].endswith(".csv"):
+                self.data = (
+                    CustomCSVDataset(data["name"], task=task).load_data(
+                        feature_column=data.get("feature_column", "text"),
+                        target_column=data.get("target_column", "label"),
+                    )
+                    if data is not None
+                    else None
+                )
+
+                if hub == "spacy" and (model == "textcat_imdb" or model is None):
+                    if model is None:
+                        logging.warning(
+                            "Using the default 'textcat_imdb' model for Spacy hub. Please provide a custom model path if desired."
+                        )
+                    model = resource_filename("langtest", "data/textcat_imdb")
 
         elif (
-            type(data) is dict
+            
+             type(data) is dict
             and hub in self.SUPPORTED_HUBS_HF_DATASET_SUMMARIZATION
             and task == "summarization"
         ):
-            self.data = HuggingFaceDataset(data["name"], task=task).load_data(
-                feature_column=data.get("feature_column", "document"),
-                target_column=data.get("target_column", "summary"),
-                split=data.get("split", "test"),
-                subset=data.get("subset", None),
-            )
+            if not data["name"].endswith(".csv"):
+                self.data = HuggingFaceDataset(data["name"], task=task).load_data(
+                    feature_column=data.get("feature_column", "document"),
+                    target_column=data.get("target_column", "summary"),
+                    split=data.get("split", "test"),
+                    subset=data.get("subset", None),
+                )
+            elif data["name"].endswith(".csv"):
+                    self.data = CustomCSVDataset(data["name"], task=task).load_data(
+                    feature_column=data.get("feature_column", "document"),
+                    target_column=data.get("target_column", "summary"),
+                )
 
         elif data is None and (task, model, hub) not in self.DEFAULTS_DATASET.keys():
             raise ValueError(
