d587254c25f57acf9ad69f1b34e51d8cd8905a96
Prikshit7766 <prikshitsharma8024@gmail.com>
2023-07-21 18:00:54 +0530

    Test(tests\test_representation.py): added test using pytest

--- tests/test_representation.py
+++ tests/test_representation.py
@@ -1,74 +1,53 @@
-import unittest
 import pandas as pd
+import pytest
 from langtest import Harness
 
 
-class RepresentationTestCase(unittest.TestCase):
+params = {
+    "spacy_ner": {
+        "task": "ner",
+        "model": "en_core_web_sm",
+        "hub": "spacy",
+    },
+    "huggingface_ner": {
+        "task": "ner",
+        "model": "dslim/bert-base-NER",
+        "hub": "huggingface",
+    },
+    "jsl_ner": {
+        "task": "ner",
+        "model": "ner.dl",
+        "hub": "johnsnowlabs",
+    },
+    "jsl_text_classification": {
+        "task": "text-classification",
+        "model": "en.sentiment.imdb.glove",
+        "hub": "johnsnowlabs",
+    },
+    "spacy_text_classification": {
+        "task": "text-classification",
+        "model": "textcat_imdb",
+        "hub": "spacy",
+    },
+    "huggingface_text_classification": {
+        "task": "text-classification",
+        "model": "lvwerra/distilbert-imdb",
+        "hub": "huggingface",
+    },
+}
+
+
+class TestRepresentation:
     """Test case class for representation tests.
 
     This test case class evaluates the representation quality of various NLP models.
     It checks the representation counts and proportions of gender, ethnicity names, labels, religions,
     and country economic data in the model outputs.
-
-    Attributes:
-        params (dict): A dictionary containing model configuration parameters.
-
-    Methods:
-        setUp(self): Initializes the test case by setting up the model parameters.
-        configure_tests(self): Configures the tests by setting the minimum count and proportion thresholds for
-                              representation evaluation.
-        test_representation_hf_ner(self): Tests the representation quality of the Huggingface NER model.
-        test_representation_spacy_ner(self): Tests the representation quality of the Spacy NER model.
-        test_representation_jsl_ner(self): Tests the representation quality of the John Snow Labs NER model.
-        test_representation_jsl_test_classification(self): Tests the representation quality of the John Snow Labs
-                                                           Text Classification model.
-        test_representation_spacy_test_classification(self): Tests the representation quality of the Spacy Text
-                                                             Classification model.
-        test_representation_huggingface_test_classification(self): Tests the representation quality of the Huggingface
-                                                                   Text Classification model.
-
     """
 
-    def setUp(self) -> None:
-        """Sets up the test case by initializing parameters."""
-
-        self.params = {
-            "spacy_ner": {
-                "task": "ner",
-                "model": "en_core_web_sm",
-                "hub": "spacy",
-            },
-            "huggingface_ner": {
-                "task": "ner",
-                "model": "dslim/bert-base-NER",
-                "hub": "huggingface",
-            },
-            "jsl_ner": {
-                "task": "ner",
-                "model": "ner.dl",
-                "hub": "johnsnowlabs",
-            },
-            "jsl_text_classification": {
-                "task": "text-classification",
-                "model": "en.sentiment.imdb.glove",
-                "hub": "johnsnowlabs",
-            },
-            "spacy_text_classification": {
-                "task": "text-classification",
-                "model": "textcat_imdb",
-                "hub": "spacy",
-            },
-            "huggingface_text_classification": {
-                "task": "text-classification",
-                "model": "lvwerra/distilbert-imdb",
-                "hub": "huggingface",
-            },
-        }
-
-    def configure_tests(self) -> None:
+    def configure_tests(self, harness):
         """Configures the test settings for the harness."""
-
-        self.harness.configure(
+        harness.configure(
             {
                 "tests": {
                     "defaults": {"min_pass_rate": 0.65},
@@ -94,68 +73,34 @@ class RepresentationTestCase(unittest.TestCase):
             }
         )
 
-    def test_representation_hf_ner(self):
-        """Test representation for Hugging Face NER model."""
+    @pytest.mark.parametrize(
+        "model_params",
+        [
+            params["jsl_text_classification"],
+            params["spacy_text_classification"],
+            params["huggingface_text_classification"],
+        ],
+    )
+    def test_representation_text_classification(self, model_params):
+        """Test representation for Text Classification models."""
 
-        harness = Harness(**self.params["huggingface_ner"])
-        self.harness = harness
-        self.configure_tests()
+        harness = Harness(**model_params)
+        self.configure_tests(harness)
         harness.data = harness.data[:50]
-        self.assertIsInstance(harness, Harness)
+        assert isinstance(harness, Harness)
         report = harness.generate().run().report()
-        self.assertIsInstance(report, pd.DataFrame)
+        assert isinstance(report, pd.DataFrame)
 
-    def test_representation_spacy_ner(self):
-        """Test representation for spaCy NER model."""
+    @pytest.mark.parametrize(
+        "model_params",
+        [params["spacy_ner"], params["huggingface_ner"], params["jsl_ner"]],
+    )
+    def test_representation_ner(self, model_params):
+        """Test representation for NER models."""
 
-        harness = Harness(**self.params["spacy_ner"])
-        self.harness = harness
-        self.configure_tests()
+        harness = Harness(**model_params)
+        self.configure_tests(harness)
         harness.data = harness.data[:50]
-        self.assertIsInstance(harness, Harness)
+        assert isinstance(harness, Harness)
         report = harness.generate().run().report()
-        self.assertIsInstance(report, pd.DataFrame)
-
-    def test_representation_jsl_ner(self):
-        """Test representation for John Snow Labs NER model."""
-
-        harness = Harness(**self.params["jsl_ner"])
-        self.harness = harness
-        self.configure_tests()
-        harness.data = harness.data[:50]
-        self.assertIsInstance(harness, Harness)
-        report = harness.generate().run().report()
-        self.assertIsInstance(report, pd.DataFrame)
-
-    def test_representation_jsl_test_classification(self):
-        """Test representation for John Snow Labs Text Classification model."""
-
-        harness = Harness(**self.params["jsl_text_classification"])
-        self.harness = harness
-        self.configure_tests()
-        harness.data = harness.data[:50]
-        self.assertIsInstance(harness, Harness)
-        report = harness.generate().run().report()
-        self.assertIsInstance(report, pd.DataFrame)
-
-    def test_representation_spacy_test_classification(self):
-        """Test representation for spaCy Text Classification model."""
-
-        harness = Harness(**self.params["spacy_text_classification"])
-        self.harness = harness
-        self.configure_tests()
-        harness.data = harness.data[:50]
-        self.assertIsInstance(harness, Harness)
-        report = harness.generate().run().report()
-        self.assertIsInstance(report, pd.DataFrame)
-
-    def test_representation_huggingface_test_classification(self):
-        """Test representation for Hugging Face Text Classification model."""
-
-        harness = Harness(**self.params["huggingface_text_classification"])
-        self.harness = harness
-        self.configure_tests()
-        harness.data = harness.data[:50]
-        self.assertIsInstance(harness, Harness)
-        report = harness.generate().run().report()
-        self.assertIsInstance(report, pd.DataFrame)
+        assert isinstance(report, pd.DataFrame)
