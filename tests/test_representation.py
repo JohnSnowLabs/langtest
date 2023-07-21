20050aecc6c1dda48770cf83a24c7b10ed0115b4
Rakshit Khajuria <rakshitraina1234@gmail.com>
2023-07-21 18:03:42 +0530

    test(test_representation.py) : Added generic test for representation

--- tests/test_representation.py
+++ tests/test_representation.py
@@ -3,50 +3,62 @@ import pytest
 from langtest import Harness
 
 
-params = {
-    "spacy_ner": {
-        "task": "ner",
-        "model": "en_core_web_sm",
-        "hub": "spacy",
-    },
-    "huggingface_ner": {
-        "task": "ner",
-        "model": "dslim/bert-base-NER",
-        "hub": "huggingface",
-    },
-    "jsl_ner": {
-        "task": "ner",
-        "model": "ner.dl",
-        "hub": "johnsnowlabs",
-    },
-    "jsl_text_classification": {
-        "task": "text-classification",
-        "model": "en.sentiment.imdb.glove",
-        "hub": "johnsnowlabs",
-    },
-    "spacy_text_classification": {
-        "task": "text-classification",
-        "model": "textcat_imdb",
-        "hub": "spacy",
-    },
-    "huggingface_text_classification": {
-        "task": "text-classification",
-        "model": "lvwerra/distilbert-imdb",
-        "hub": "huggingface",
-    },
-}
-
-
 class TestRepresentation:
-    """Test case class for representation tests.
+    """
+    TestRepresentation class for evaluating the representation quality of various NLP models.
 
-    This test case class evaluates the representation quality of various NLP models.
-    It checks the representation counts and proportions of gender, ethnicity names, labels, religions,
+    This class checks the representation counts and proportions of gender, ethnicity names, labels, religions,
     and country economic data in the model outputs.
+
+    Attributes:
+        params (dict): Dictionary containing different NLP models and their configurations.
+
+    Methods:
+        configure_tests(harness): Configures the test settings for the harness.
+        test_representation(model_params): Generic test representation for NER and Text Classification models.
     """
 
+    params = {
+        "spacy_ner": {
+            "task": "ner",
+            "model": "en_core_web_sm",
+            "hub": "spacy",
+        },
+        "huggingface_ner": {
+            "task": "ner",
+            "model": "dslim/bert-base-NER",
+            "hub": "huggingface",
+        },
+        "jsl_ner": {
+            "task": "ner",
+            "model": "ner.dl",
+            "hub": "johnsnowlabs",
+        },
+        "jsl_text_classification": {
+            "task": "text-classification",
+            "model": "en.sentiment.imdb.glove",
+            "hub": "johnsnowlabs",
+        },
+        "spacy_text_classification": {
+            "task": "text-classification",
+            "model": "textcat_imdb",
+            "hub": "spacy",
+        },
+        "huggingface_text_classification": {
+            "task": "text-classification",
+            "model": "lvwerra/distilbert-imdb",
+            "hub": "huggingface",
+        },
+    }
+
     def configure_tests(self, harness):
-        """Configures the test settings for the harness."""
+        """
+        Configures the test settings for the harness.
+
+        Args:
+            harness (Harness): The Harness instance to configure the tests.
+        """
+
         harness.configure(
             {
                 "tests": {
@@ -73,34 +85,21 @@ class TestRepresentation:
             }
         )
 
-    @pytest.mark.parametrize(
-        "model_params",
-        [
-            params["jsl_text_classification"],
-            params["spacy_text_classification"],
-            params["huggingface_text_classification"],
-        ],
-    )
-    def test_representation_text_classification(self, model_params):
-        """Test representation for Text Classification models."""
+    @pytest.mark.parametrize("model_params", params.values())
+    def test_representation(self, model_params):
+        """
+        Generic test representation for NER and Text Classification models.
+
+        Args:
+            model_params (dict): Dictionary containing the parameters for the NLP model.
+
+        Raises:
+            AssertionError: If the generated report is not an instance of pd.DataFrame.
+        """
 
         harness = Harness(**model_params)
         self.configure_tests(harness)
-        harness.data = harness.data[:50]
-        assert isinstance(harness, Harness)
-        report = harness.generate().run().report()
-        assert isinstance(report, pd.DataFrame)
-
-    @pytest.mark.parametrize(
-        "model_params",
-        [params["spacy_ner"], params["huggingface_ner"], params["jsl_ner"]],
-    )
-    def test_representation_ner(self, model_params):
-        """Test representation for NER models."""
-
-        harness = Harness(**model_params)
-        self.configure_tests(harness)
-        harness.data = harness.data[:50]
+        harness.data = harness.data[:1]
         assert isinstance(harness, Harness)
         report = harness.generate().run().report()
         assert isinstance(report, pd.DataFrame)
