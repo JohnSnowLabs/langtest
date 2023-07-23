6f59b2980d7544e8f1f53d81e2cb464235d33e94
Rakshit Khajuria <rakshitraina1234@gmail.com>
2023-07-24 02:33:34 +0530

    tests(test_representation): RepresentationTestCase structure

--- tests/test_representation.py
+++ tests/test_representation.py
@@ -1,105 +1,60 @@
-import pandas as pd
-import pytest
-from langtest import Harness
+import unittest
+from langtest.transform.representation import *
+from langtest.utils.custom_types.sample import *
 
 
-class TestRepresentation:
+class RepresentationTestCase(unittest.TestCase):
     """
-    TestRepresentation class for evaluating the representation quality of various NLP models.
+    Test case for representation classes.
 
-    This class checks the representation counts and proportions of gender, ethnicity names, labels, religions,
-    and country economic data in the model outputs.
-
-    Attributes:
-        params (dict): Dictionary containing different NLP models and their configurations.
-
-    Methods:
-        configure_tests(harness): Configures the test settings for the harness.
-        test_representation(model_params): Generic test representation for NER and Text Classification models.
     """
 
-    params = {
-        "spacy_ner": {
-            "task": "ner",
-            "model": "en_core_web_sm",
-            "hub": "spacy",
-        },
-        "huggingface_ner": {
-            "task": "ner",
-            "model": "dslim/bert-base-NER",
-            "hub": "huggingface",
-        },
-        "jsl_ner": {
-            "task": "ner",
-            "model": "ner.dl",
-            "hub": "johnsnowlabs",
-        },
-        "jsl_text_classification": {
-            "task": "text-classification",
-            "model": "en.sentiment.imdb.glove",
-            "hub": "johnsnowlabs",
-        },
-        "spacy_text_classification": {
-            "task": "text-classification",
-            "model": "textcat_imdb",
-            "hub": "spacy",
-        },
-        "huggingface_text_classification": {
-            "task": "text-classification",
-            "model": "lvwerra/distilbert-imdb",
-            "hub": "huggingface",
-        },
-    }
-
-    def configure_tests(self, harness):
+    def setUp(self):
         """
-        Configures the test settings for the harness.
-
-        Args:
-            harness (Harness): The Harness instance to configure the tests.
+        Set up the test case.
         """
+        # Create sample data
+        self.sentences = [
+            SequenceClassificationSample(
+                original="Just as a reminder to anyone just now reading the comments on this excellent BBC mini-series, published in 1981, it was not available on DVD until the last few years. Since then, it has become available, but initially only in the British format (for which I bought an 'international' DVD player, which you have to hack--illegally, I suspect, to see it), but the series is now available through amazon.com--3 discs-- for between $19-21, to be viewed on DVD in the US format, no hacking. There were 41 reviews, average 5 stars. This mini-series is one of the very best on Oppenheimer, or the Manhattan Project, or virtually anything produced by the BBC."
+            )
+        ]
 
-        harness.configure(
-            {
-                "tests": {
-                    "defaults": {"min_pass_rate": 0.65},
-                    "representation": {
-                        "min_gender_representation_count": {"min_count": 10},
-                        "min_ethnicity_name_representation_count": {"min_count": 10},
-                        "min_label_representation_count": {"min_count": 10},
-                        "min_religion_name_representation_count": {"min_count": 10},
-                        "min_country_economic_representation_count": {"min_count": 10},
-                        "min_gender_representation_proportion": {"min_proportion": 0.1},
-                        "min_ethnicity_name_representation_proportion": {
-                            "min_proportion": 0.1
-                        },
-                        "min_label_representation_proportion": {"min_proportion": 0.1},
-                        "min_religion_name_representation_proportion": {
-                            "min_proportion": 0.1
-                        },
-                        "min_country_economic_representation_proportion": {
-                            "min_proportion": 0.1
-                        },
-                    },
-                }
-            }
-        )
+        # Define the gender representation configurations
+        self.gender_representation = {
+            "min_gender_representation_count": {"min_count": 5},
+            "min_gender_representation_proportion": {"min_proportion": 0.1},
+        }
 
-    @pytest.mark.parametrize("model_params", params.values())
-    def test_representation(self, model_params):
-        """
-        Generic test representation for NER and Text Classification models.
+        self.ethnicity_representation = {
+            "min_ethnicity_name_representation_count": {"min_count": 10},
+            "min_ethnicity_name_representation_proportion": {"min_proportion": 0.1},
+        }
 
-        Args:
-            model_params (dict): Dictionary containing the parameters for the NLP model.
+        self.religion_representation = {
+            "min_religion_name_representation_count": {"min_count": 10},
+            "min_religion_name_representation_proportion": {"min_proportion": 0.1},
+        }
+
+        self.country_representation = {
+            "min_country_economic_representation_count": {"min_count": 10},
+            "min_country_economic_representation_proportion": {"min_proportion": 0.1},
+        }
+
+    async def run_transform(self, representation_class, transform_result):
+        """Run the transform asynchronously.
+
+        Parameters:
+        - representation_class (type): The representation class to be used.
+        - transform_result (list): The result of the transform operation.
+
+        Returns:
+        - list: The transformed result.
 
         Raises:
-            AssertionError: If the generated report is not an instance of pd.DataFrame.
+        - Exception: If an error occurs during the asynchronous execution.
         """
-
-        harness = Harness(**model_params)
-        self.configure_tests(harness)
-        harness.data = harness.data[:50]
-        assert isinstance(harness, Harness)
-        report = harness.generate().run().report()
-        assert isinstance(report, pd.DataFrame)
+        result = await representation_class.run(
+            transform_result, model="lvwerra/distilbert-imdb", raw_data=self.sentences
+        )
+        return result
