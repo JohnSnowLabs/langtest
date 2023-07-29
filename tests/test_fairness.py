c133c933bdf3f82254bdbdf89bb3a001b9af3f97
Prikshit7766 <prikshitsharma8024@gmail.com>
2023-07-30 02:00:53 +0530

    test(test_fairness): added pytest for fairness

---
+++ tests/test_fairness.py
@@ -0,0 +1,107 @@
+import pytest
+
+from langtest.transform.fairness import (
+    BaseFairness,
+    MinGenderF1Score,
+    MaxGenderF1Score,
+)
+from langtest.utils.custom_types import SequenceLabel, Span
+from langtest.utils.custom_types.output import (
+    MaxScoreSample,
+    MinScoreSample,
+    NEROutput,
+    NERPrediction,
+    SequenceClassificationOutput,
+)
+
+
+class Testfairness:
+    """A test suite for evaluating the transformation process of various fairnesss.
+
+    This test suite ensures that the fairnesss can successfully transform input data
+    and produce valid results.
+
+    The fairnesss tested include Genderfairness, Ethnicityfairness,
+    Religionfairness, and CountryEconomicfairness.
+
+    Attributes:
+        fairness_config (Dict)
+    """
+
+    fairness_config = {
+        "min_gender_f1_score": {"min_score": 0.66},
+        "max_gender_f1_score": {"max_score": 0.60},
+    }
+
+    @pytest.fixture
+    def sample_data(self):
+        """A fixture providing sample data for the fairness transformation tests.
+
+        Returns:
+            list: A list containing sample SequenceClassificationSample instances.
+        """
+        return {
+            "text-classification": [
+                SequenceClassificationSample(
+                    original="The last good ernest movie, and the best at that. how can you not laugh at least once at this movie. the last line is a classic, as is ernest's gangster impressions, his best moment on film. this has his best lines and is a crowning achievement among the brainless screwball comedies.",
+                    expected_results=SequenceClassificationOutput(
+                        predictions=[SequenceLabel(label="Positive", score=1.0)]
+                    ),
+                ),
+                SequenceClassificationSample(
+                    original="After my 6 year old daughter began taking riding lessons I started looking for horse movies for her. I had always heard of National Velvet but had never seen it. Boy am I glad I bought it! It's become a favorite of mine, my 6 year old AND my 2 year old. It's a shame movies like this aren't made anymore.",
+                    expected_results=SequenceClassificationOutput(
+                        predictions=[SequenceLabel(label="Positive", score=1.0)]
+                    ),
+                ),
+            ],
+            "ner": [
+                NERSample(
+                    original="Attendance : 3,000",
+                    expected_results=NEROutput(
+                        predictions=[
+                            NERPrediction(
+                                entity="CARDINAL",
+                                span=Span(start=13, end=18, word="3,000"),
+                            )
+                        ]
+                    ),
+                )
+            ],
+        }
+
+    @pytest.mark.parametrize(
+        "fairness",
+        [
+            MinGenderF1Score,
+            MaxGenderF1Score,
+        ],
+    )
+    def test_transform(self, fairness: BaseFairness, sample_data) -> None:
+        """
+        Test case for fairness classes.
+
+        Args:
+            fairness (Type[fairness]): The fairness class to be tested.
+            sample_data (List]): A list containing sample instances.
+
+        Returns:
+            None
+
+        Raises:
+            AssertionError: If the transformation or the final result is invalid.
+        """
+        for alias in fairness.alias_name:
+            print(alias)
+            for task in fairness.supported_tasks:
+                transform_results = fairness.transform(
+                    alias, sample_data[task], self.fairness_config[alias]
+                )
+                print(transform_results)
+                assert isinstance(transform_results, list)
+
+                for _, result in zip(sample_data, transform_results):
+                    print(transform_results)
+                    assert isinstance(result, MaxScoreSample) or isinstance(
+                        result, MinScoreSample
+                    )
