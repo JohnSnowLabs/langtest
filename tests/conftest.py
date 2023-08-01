42fcd87d5de98fb69587c7f5483645cbbfbfd78f
JulesBelveze <jules.belveze@hotmail.fr>
2023-08-01 10:37:53 +0200

    tests: fix metaflow test config

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import os
 import pytest
 import json
@@ -5,7 +6,12 @@ import json
 
 def pytest_sessionstart():
     """Called after the Session object has been created"""
-    os.environ["METAFLOW_PROFILE"] = "local"
+    homewd = str(Path.home())
+    metaflow_config = os.path.join(homewd, ".metaflowconfig")
+    os.makedirs(metaflow_config, exist_ok=True)
+
+    with open(os.path.join(metaflow_config, "config_test.json"), "w") as writer:
+        json.dump({"METAFLOW_DEFAULT_DATASTORE": "local"}, writer)
 
 
 @pytest.fixture(scope="session", autouse=True)
