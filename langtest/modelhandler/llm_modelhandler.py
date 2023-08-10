6ae383c7ec227ed04e8d00f7a285cd67d258a72c
Arshaan <arshaan666@gmail.com>
2023-08-10 19:55:18 +0530

    update llm_modelhandler

--- langtest/modelhandler/llm_modelhandler.py
+++ langtest/modelhandler/llm_modelhandler.py
@@ -161,3 +161,16 @@ class PretrainedModelForToxicity(PretrainedModelForQA, _ModelHandler):
     """
 
     pass
+
+
+class PretrainedModelForClinicalTests(PretrainedModelForQA, _ModelHandler):
+    """A class representing a pretrained model for clinical tests.
+
+    Inherits:
+        PretrainedModelForQA: The base class for pretrained models.
+    """
+
+    pass
+
+
+
