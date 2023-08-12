9eb8cfde7b59e9a219b1fdd1d155e387d2628d4b
Rakshit Khajuria <rakshitraina1234@gmail.com>
2023-08-12 15:24:16 +0530

    test(test_representation): Added assertions

--- tests/test_representation.py
+++ tests/test_representation.py
@@ -28,6 +28,15 @@ from langtest.utils.custom_types.sample import (
 
 
 class TestRepresentation:
+    """A test suite for evaluating the transformation process of various representations.
+    This test suite ensures that the representations can successfully transform input data
+    and produce valid results.
+    The representations tested include GenderRepresentation, EthnicityRepresentation,
+    ReligionRepresentation, and CountryEconomicRepresentation.
+    Attributes:
+        representation_config (Dict)
+    """
+
     representation_config = {
         "min_gender_representation_count": {"min_count": 5},
         "min_gender_representation_proportion": {"min_proportion": 0.1},
@@ -43,6 +52,10 @@ class TestRepresentation:
 
     @pytest.fixture
     def sample_data(self):
+        """A fixture providing sample data for the representation transformation tests.
+        Returns:
+            list: A list containing sample SequenceClassificationSample instances.
+        """
         return {
             "text-classification": [
                 SequenceClassificationSample(
@@ -100,4 +113,48 @@ class TestRepresentation:
                     prompt="Below is a lineup composed of players whose fantasy equity scores \u2014 a useful measurement \u2014"
                 )
             ],
+            "translation": [
+                TranslationSample(
+                    original="John Snow Labs is a healthcare company specializing in accelerating progress in data "
+                    "science.",
+                    expected_results=TranslationOutput(
+                        translation_text="John Snow Labs est une entreprise de santé spécialisée dans l'accélération "
+                        "des progrès en science des données."
+                    ),
+                )
+            ],
         }
+
+    @pytest.mark.parametrize(
+        "representation",
+        [
+            GenderRepresentation,
+            EthnicityRepresentation,
+            ReligionRepresentation,
+            CountryEconomicRepresentation,
+            LabelRepresentation,
+        ],
+    )
+    def test_transform(self, representation: BaseRepresentation, sample_data) -> None:
+        """
+        Test case for representation classes.
+        Args:
+            representation (Type[Representation]): The representation class to be tested.
+            sample_data (List]): A list containing sample instances.
+        Returns:
+            None
+        Raises:
+            AssertionError: If the transformation or the final result is invalid.
+        """
+        for alias in representation.alias_name:
+            for task in representation.supported_tasks:
+                transform_results = representation.transform(
+                    alias, sample_data[task], self.representation_config[alias]
+                )
+
+                assert isinstance(transform_results, list)
+
+                for sample, result in zip(sample_data, transform_results):
+                    assert isinstance(result, MinScoreQASample) or isinstance(
+                        result, MinScoreSample
+                    )
