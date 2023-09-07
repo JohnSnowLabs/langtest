771c9193db8e39dfbef23af28760b4715a60e953
Ali Tarik <alimirik55@gmail.com>
2023-09-07 12:25:52 +0300

    add other datasets

--- langtest/utils/custom_types/sample.py
+++ langtest/utils/custom_types/sample.py
@@ -495,7 +495,13 @@ class QASample(BaseQASample):
         from ...transform.constants import qa_prompt_template
         from langchain.prompts import PromptTemplate
 
-        if self.dataset_name in ["BoolQ"] and (self.actual_results == self.expected_results):
+        if self.dataset_name in [
+            "BoolQ",
+            "asdiv",
+            "LogiQA",
+            "MMLU",
+            "OpenBookQA",
+        ] and (self.actual_results == self.expected_results):
             return True
 
         if "llm" in str(type(llm_model.model_class)):
