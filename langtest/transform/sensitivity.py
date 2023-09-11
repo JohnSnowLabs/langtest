17637f610d7967b1350ef80926c6a49a39ef43c2
Prikshit7766 <prikshitsharma8024@gmail.com>
2023-09-11 17:12:53 +0530

    task(sensitivity.py): added SensitivityNegation

--- langtest/transform/sensitivity.py
+++ langtest/transform/sensitivity.py
@@ -79,3 +79,51 @@ class BaseSensitivity(ABC):
         """
         created_task = asyncio.create_task(cls.run(sample_list, model, **kwargs))
         return created_task
+
+
+class SensitivityNegation(BaseSensitivity):
+    """A class for negating sensitivity-related phrases in the input text.
+
+    This class identifies common sensitivity-related phrases such as 'is', 'was', 'are', and 'were' in the input text
+    and replaces them with their negations to make the text less sensitive.
+
+    Attributes:
+        alias_name (str): The alias name for this sensitivity transformation.
+
+    Methods:
+        transform(sample_list: List[Sample]) -> List[Sample]: Applies the sensitivity negation transformation to a list
+            of samples.
+    """
+
+    alias_name = "sensitivity_negation"
+
+    @staticmethod
+    def transform(sample_list: List[Sample]) -> List[Sample]:
+        def apply_transformation(perturbed_text):
+            """
+            Filters the dataset for 'is', 'was', 'are', or 'were' and negates them.
+
+            Args:
+                perturbed_text (str): The input text to be transformed.
+
+            Returns:
+                str: The transformed text with sensitivity-related phrases negated.
+            """
+            transformations = {
+                " is ": " is not ",
+                " was ": " was not ",
+                " are ": " are not ",
+                " were ": " were not ",
+            }
+
+            for keyword, replacement in transformations.items():
+                if keyword in perturbed_text and replacement not in perturbed_text:
+                    perturbed_text = perturbed_text.replace(keyword, replacement, 1)
+
+            return perturbed_text
+
+        for idx, sample in enumerate(sample_list):
+            if isinstance(sample, str):
+                sample_list[idx] = apply_transformation(sample)
+
+        return sample_list
