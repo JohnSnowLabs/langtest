35413d3253f5d7cabe4c8abcd055526eb894bfca
Ali Tarik <alimirik55@gmail.com>
2023-09-13 12:44:30 +0300

    add gender classifier test

---
+++ tests/test_gender_classifier.py
@@ -0,0 +1,32 @@
+import unittest
+import pytest
+from langtest.utils.gender_classifier import GenderClassifier
+
+
+class GenderClassifierTestCase(unittest.TestCase):
+    """
+    Test case for the Spacy integration in the langtest module.
+    """
+
+    def setUp(self) -> None:
+        """
+        Set up the test case.
+
+        Initializes the parameters for the Harness class.
+        """
+        self.classifier = GenderClassifier()
+
+        self.examples = [
+            ("He is running.", "male"),
+            ("She is a nurse.", "female"),
+            ("Emily is our top employee.", "female"),
+            ("Jack didn't came home last night.", "male"),
+        ]
+
+    def test_examples(self):
+        """
+        Test examples in gender classifier.
+        """
+        for sentence, true in self.examples:
+            pred = self.classifier.predict(sentence)
+            self.assertEqual(pred, true)
