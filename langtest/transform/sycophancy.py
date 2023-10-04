58e3536e43d54f0a6973367c3ffae30ace7c8f28
Kalyan Chakravarthy <chakravarthik27@gmail.com>
2023-10-04 12:01:12 +0530

    resolved: import error from sycophancy

--- langtest/transform/sycophancy.py
+++ langtest/transform/sycophancy.py
@@ -1,7 +1,7 @@
 import asyncio
 from abc import ABC, abstractmethod
 from typing import List
-from langtest.modelhandler.modelhandler import ModelFactory
+from langtest.modelhandler.modelhandler import ModelAPI
 from ..utils.custom_types import Sample
 import re
 from .constants import SCHOOLS, NAMES
@@ -39,13 +39,13 @@ class BaseSycophancy(ABC):
     @staticmethod
     @abstractmethod
     async def run(
-        sample_list: List[Sample], model: ModelFactory, **kwargs
+        sample_list: List[Sample], model: ModelAPI, **kwargs
     ) -> List[Sample]:
         """Abstract method that implements the sycophancy measure.
 
         Args:
             sample_list (List[Sample]): The input data to be transformed.
-            model (ModelFactory): The model to be used for evaluation.
+            model (ModelAPI): The model to be used for evaluation.
             **kwargs: Additional arguments to be passed to the sycophancy measure.
 
         Returns:
@@ -68,12 +68,12 @@ class BaseSycophancy(ABC):
         return sample_list
 
     @classmethod
-    async def async_run(cls, sample_list: List[Sample], model: ModelFactory, **kwargs):
+    async def async_run(cls, sample_list: List[Sample], model: ModelAPI, **kwargs):
         """Creates a task to run the sycophancy measure.
 
         Args:
             sample_list (List[Sample]): The input data to be transformed.
-            model (ModelFactory): The model to be used for evaluation.
+            model (ModelAPI): The model to be used for evaluation.
             **kwargs: Additional arguments to be passed to the sycophancy measure.
 
         Returns:
