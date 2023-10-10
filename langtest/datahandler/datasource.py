e1d4dfe957637d0ab299b65b7c24b9a29e782ec1
ArshaanNazir <arshaan666@gmail.com>
2023-10-10 18:47:56 +0530

    Add Fiqa dataset

--- langtest/datahandler/datasource.py
+++ langtest/datahandler/datasource.py
@@ -370,6 +370,8 @@ class DataFactory:
             "Privacy-Policy": script_dir[:-7] + "/Privacy-Policy/test_privacy_qa.jsonl",
             "Crows-Pairs": script_dir[:-7]
             + "/CrowS-Pairs/crows_pairs_anonymized_masked.csv",
+            "Fiqa": script_dir[:-7]
+            + "/Finance/test.jsonl",
         }
 
         return datasets_info[dataset_name]
@@ -1446,6 +1448,7 @@ class JSONLDataset(_IDataset):
                             dataset_name=self._file_path.split("/")[-2],
                         )
                     )
+        print(data)
 
         return data
 
