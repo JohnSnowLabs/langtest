5301720cb774d2b4d1790329c6876205fba47469
Ali Tarik <alimirik55@gmail.com>
2023-10-16 12:21:31 +0300

    add stereoset datahandler

--- langtest/datahandler/datasource.py
+++ langtest/datahandler/datasource.py
@@ -35,6 +35,7 @@ from langtest.utils.custom_types import (
     FactualitySample,
     SycophancySample,
     CrowsPairsSample,
+    StereoSetSample,
 )
 from ..utils.lib_manager import try_import_lib
 from ..transform.constants import DATASETS
@@ -95,6 +96,15 @@ COLUMN_MAPPER = {
         "sentence": ["sentence"],
         "mask1": ["mask1"],
         "mask2": ["mask2"],
+    },
+    "stereoset": {
+        "type": ["type"],
+        "target": ["target"],
+        "bias_type": ["bias_type"],
+        "context": ["context"],
+        "stereotype": ["stereotype"],
+        "anti-stereotype": ["anti-stereotype"],
+        "unrelated": ["unrelated"],
     },
 }
 
@@ -368,6 +378,7 @@ class DataFactory:
             "Privacy-Policy": script_dir[:-7] + "/Privacy-Policy/test_privacy_qa.jsonl",
             "Crows-Pairs": script_dir[:-7]
             + "/CrowS-Pairs/crows_pairs_anonymized_masked.csv",
+            "StereoSet": script_dir[:-7] + "/StereoSet/stereoset.jsonl",
         }
 
         return datasets_info[dataset_name]
@@ -1263,6 +1274,7 @@ class JSONLDataset(_IDataset):
         "wino-bias",
         "legal-tests",
         "factuality-test",
+        "stereoset",
     ]
     COLUMN_NAMES = {task: COLUMN_MAPPER[task] for task in supported_tasks}
 
