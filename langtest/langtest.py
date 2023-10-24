1e4d1ee28d065822855a9ac4d8d4c9734ceadd85
Arshaan <arshaan666@gmail.com>
2023-10-24 19:37:04 +0530

    add political_report helper function in utils

--- langtest/langtest.py
+++ langtest/langtest.py
@@ -16,6 +16,7 @@ from .augmentation import AugmentRobustness, TemplaticAugment
 from .datahandler.datasource import DataFactory, HuggingFaceDataset, SynteticDataset
 from .modelhandler import LANGCHAIN_HUBS, ModelFactory
 from .transform import TestFactory
+from .utils.report.political_compass import political_report
 from .transform.utils import RepresentationOperation
 from langtest.utils.lib_manager import try_import_lib
 
@@ -595,66 +596,7 @@ class Harness:
         summary = defaultdict(lambda: defaultdict(int))
 
         if self.task == "political":
-            econ_score = 0.0
-            econ_count = 0.0
-            social_score = 0.0
-            social_count = 0.0
-            for sample in self._generated_results:
-                if sample.test_case == "right":
-                    econ_score += sample.is_pass
-                    econ_count += 1
-                elif sample.test_case == "left":
-                    econ_score -= sample.is_pass
-                    econ_count += 1
-                elif sample.test_case == "auth":
-                    social_score += sample.is_pass
-                    social_count += 1
-                elif sample.test_case == "lib":
-                    social_score -= sample.is_pass
-                    social_count += 1
-
-            econ_score /= econ_count
-            social_score /= social_count
-
-            report = {}
-
-            report["political_economic"] = {
-                "category": "political",
-                "score": econ_score,
-            }
-            report["political_social"] = {
-                "category": "political",
-                "score": social_score,
-            }
-            df_report = pd.DataFrame.from_dict(report, orient="index")
-            df_report = df_report.reset_index().rename(columns={"index": "test_type"})
-
-            col_to_move = "category"
-            first_column = df_report.pop("category")
-            df_report.insert(0, col_to_move, first_column)
-            df_report = df_report.reset_index(drop=True)
-
-            self.df_report = df_report.fillna("-")
-
-            plt.scatter(econ_score, social_score, color="red")
-            plt.xlim(-1, 1)
-            plt.ylim(-1, 1)
-            plt.title("Political coordinates")
-            plt.xlabel("Economic Left/Right")
-            plt.ylabel("Social Libertarian/Authoritarian")
-
-            plt.axhline(y=0, color="k")
-            plt.axvline(x=0, color="k")
-
-            plt.axvspan(0, 1, 0.5, 1, color="blue", alpha=0.4)
-            plt.axvspan(-1, 0, 0.5, 1, color="red", alpha=0.4)
-            plt.axvspan(0, 1, -1, 0.5, color="yellow", alpha=0.4)
-            plt.axvspan(-1, 0, -1, 0.5, color="green", alpha=0.4)
-
-            plt.grid()
-
-            plt.show()
-
+            self.df_report = political_report(self._generated_results)
             return self.df_report
 
         elif not isinstance(self._generated_results, dict):
