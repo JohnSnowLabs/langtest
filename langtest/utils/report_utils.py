1da2978f91795d913bc7806edf62286a215b057f
Prikshit7766 <prikshitsharma8024@gmail.com>
2023-10-30 17:32:12 +0530

    updated error codes in report_utils

--- langtest/utils/report_utils.py
+++ langtest/utils/report_utils.py
@@ -2,7 +2,7 @@ import datetime
 import pandas as pd
 import matplotlib.pyplot as plt
 from typing import Dict, List, Union
-
+from ..errors import Errors
 from langtest.tasks import TaskManager
 
 
@@ -326,27 +326,25 @@ def save_format(format: str, save_dir: str, df_report: pd.DataFrame):
 
     elif format == "dict":
         if save_dir is None:
-            raise ValueError('You need to set "save_dir" parameter for this format.')
+            raise ValueError(Errors.E012)
 
         df_report.to_json(save_dir)
 
     elif format == "excel":
         if save_dir is None:
-            raise ValueError('You need to set "save_dir" parameter for this format.')
+            raise ValueError(Errors.E012)
         df_report.to_excel(save_dir)
     elif format == "html":
         if save_dir is None:
-            raise ValueError('You need to set "save_dir" parameter for this format.')
+            raise ValueError(Errors.E012)
         df_report.to_html(save_dir)
     elif format == "markdown":
         if save_dir is None:
-            raise ValueError('You need to set "save_dir" parameter for this format.')
+            raise ValueError(Errors.E012)
         df_report.to_markdown(save_dir)
     elif format == "text" or format == "csv":
         if save_dir is None:
-            raise ValueError('You need to set "save_dir" parameter for this format.')
+            raise ValueError(Errors.E012)
         df_report.to_csv(save_dir)
     else:
-        raise ValueError(
-            f'Report in format "{format}" is not supported. Please use "dataframe", "excel", "html", "markdown", "text", "dict".'
-        )
+        raise ValueError(Errors.E013)
