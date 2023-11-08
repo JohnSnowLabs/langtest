0bc85511dd31250682bbbe0a83c8c03a78eefc2a
Kalyan Chakravarthy <chakravarthik27@gmail.com>
2023-11-08 17:21:12 +0530

    Remove commented out code and update supported tasks for FactualityTestFactory

--- langtest/transform/__init__.py
+++ langtest/transform/__init__.py
@@ -1706,89 +1706,6 @@ class SensitivityTestFactory(ITests):
         return tests
 
 
-# class StereoTypeFactory(ITests):
-#     """Factory class for the wino-bias and crows tests"""
-
-#     alias_name = "stereotype"
-#     supported_tasks = [
-#         "wino-bias",
-#         "crows-pairs",
-#         "fill-mask",
-#         "question-answering",
-#     ]
-
-#     def __init__(self, data_handler: List[Sample], tests: Dict = None, **kwargs) -> None:
-#         """Initializes the wion-bias tests"""
-
-#         self.data_handler = data_handler
-#         self.tests = tests
-#         self.kwargs = kwargs
-
-#     def transform(self) -> List[Sample]:
-#         """Nothing to use transform for no longer to generating testcases.
-
-#         Returns:
-#             Empty list
-
-#         """
-#         for sample in self.data_handler:
-#             sample.test_type = "wino-bias"
-#             sample.category = "stereotype"
-#             if "diff_threshold" in self.tests["wino-bias"].keys():
-#                 sample.diff_threshold = self.tests["wino-bias"]["diff_threshold"]
-#         return self.data_handler
-
-#     @classmethod
-#     async def run(
-#         cls, sample_list: List[Sample], model: ModelAPI, **kwargs
-#     ) -> List[Sample]:
-#         """Runs the wino-bias tests
-
-#         Args:
-#             sample_list (List[Sample]): The input data to be transformed.
-#             model (ModelAPI): The model to be used for evaluation.
-#             **kwargs: Additional arguments to be passed to the wino-bias tests
-
-#         Returns:
-#             List[Sample]: The transformed data based on the implemented wino-bias tests
-
-#         """
-#         task = asyncio.create_task(cls.async_run(sample_list, model, **kwargs))
-#         return task
-
-#     @classmethod
-#     def available_tests(cls) -> Dict[str, str]:
-#         """Returns the empty dict, no wino-bias tests
-
-#         Returns:
-#             Dict[str, str]: Empty dict, no wino-bias tests
-#         """
-#         return {"wino-bias": cls}
-
-#     async def async_run(sample_list: List[Sample], model: ModelAPI, *args, **kwargs):
-#         """Runs the wino tests
-
-#         Args:
-#             sample_list (List[Sample]): The input data to be transformed.
-#             model (ModelAPI): The model to be used for evaluation.
-#             **kwargs: Additional arguments to be passed to the wino-bias tests
-
-#         Returns:
-#             List[Sample]: The transformed data based on the implemented wino-bias tests
-
-#         """
-#         progress = kwargs.get("progress_bar", False)
-#         for sample in sample_list["wino-bias"]:
-#             if sample.state != "done":
-#                 if hasattr(sample, "run"):
-#                     sample_status = sample.run(model, **kwargs)
-#                     if sample_status:
-#                         sample.state = "done"
-#             if progress:
-#                 progress.update(1)
-#         return sample_list["wino-bias"]
-
-
 class StereoTypeFactory(ITests):
     """Factory class for the crows-pairs or wino-bias tests"""
 
@@ -2077,9 +1994,7 @@ class FactualityTestFactory(ITests):
     """Factory class for factuality test"""
 
     alias_name = "factuality"
-    supported_tasks = [
-        "factuality-test",
-    ]
+    supported_tasks = ["factuality-test", "question-answering"]
 
     def __init__(self, data_handler: List[Sample], tests: Dict = None, **kwargs) -> None:
         """Initializes the factuality-test"""
