17d9cfc954dcbfe7bc5e84234049f6a789277332
Prikshit7766 <prikshitsharma8024@gmail.com>
2023-12-13 23:12:01 +0530

    hf_utils.py: added HuggingFacePipeline to load transformers

--- langtest/utils/hf_utils.py
+++ langtest/utils/hf_utils.py
@@ -1,11 +1,11 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM, AutoModelForSeq2SeqLM
-import os
-from ..errors import Errors
-from huggingface_hub import login
-import random
-from typing import List, Tuple, Dict
-import importlib
+import logging
+from typing import Any, List, Optional
+import importlib.util
+from ..errors import Errors, Warnings
 from langtest.utils.lib_manager import try_import_lib
+import os
+import random
+from typing import Tuple, Dict
 
 
 class GatedRepoAccessError(Exception):
@@ -31,6 +31,16 @@ def get_model_n_tokenizer(model_name):
     Raises:
         GatedRepoAccessError: If there is an attempt to access a gated repository without proper authorization.
     """
+    try:
+        from transformers import (
+            AutoModelForCausalLM,
+            AutoModelForSeq2SeqLM,
+            AutoTokenizer,
+        )
+        from huggingface_hub import login
+    except ImportError:
+        raise ValueError(Errors.E085)
+    
     if "HUGGINGFACEHUB_API_TOKEN" in os.environ:
         login(os.environ["HUGGINGFACEHUB_API_TOKEN"])
 
@@ -123,3 +133,109 @@ def build_dataset(
             dict_[text] = natural_language_labels[label]
 
     return train_dict, test_dict, has_validation
+
+
+class HuggingFacePipeline:
+    def __init__(
+        self,
+        model_id: Optional[str] = None,
+        task: Optional[str] = None,
+        device: Optional[int] = -1,
+        pipeline: Optional[Any] = None,
+        **kwargs: Any,
+    ):
+        self.model_id = model_id
+        if pipeline:
+            self.pipeline = pipeline
+        else:
+            self.pipeline = self._initialize_pipeline(model_id, task, device, **kwargs)
+            
+    def _initialize_pipeline(self, model_id: str, task: str, device: Optional[int], **kwargs: Any) -> Any:
+        try:
+            from transformers import (
+                AutoModelForCausalLM,
+                AutoModelForSeq2SeqLM,
+                AutoTokenizer,
+            )
+            from transformers import pipeline as hf_pipeline
+        except ImportError:
+            raise ValueError(Errors.E085)
+
+        tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+        # Set the pad_token_id for the tokenizer
+        tokenizer.pad_token_id = tokenizer.eos_token_id
+
+        try:
+            if task == "text-generation":
+                model = AutoModelForCausalLM.from_pretrained(model_id)
+            elif task in ("text2text-generation", "summarization"):
+                model = AutoModelForSeq2SeqLM.from_pretrained(model_id)
+            else:
+                raise ValueError(Errors.E086.format(task=task))
+        except ImportError as e:
+            raise ValueError(
+                Errors.E087.format(task=task)
+            ) from e
+
+        if (
+            getattr(model, "is_loaded_in_4bit", False)
+            or getattr(model, "is_loaded_in_8bit", False)
+        ) and device is not None:
+            logging.warning(Warnings.W015.format(device=device))
+            device = None
+
+        if device is not None and importlib.util.find_spec("torch") is not None:
+            import torch
+
+            cuda_device_count = torch.cuda.device_count()
+            if device < -1 or (device >= cuda_device_count):
+                raise ValueError(Errors.E088.format(device=device,cuda_device_count=cuda_device_count )
+                )
+            if device < 0 and cuda_device_count > 0:
+                logging.warning(Warnings.W016.format(cuda_device_count=cuda_device_count))
+
+
+        return hf_pipeline(
+            task=task,
+            model=model,
+            tokenizer=tokenizer,
+            device=device,
+            **kwargs,
+        )
+
+
+    def _generate(self, prompts: List[str]) -> List[str]:
+        text_generations: List[str] = []
+
+        for prompt in prompts:
+            response = self.pipeline(prompt)
+
+            if isinstance(response, list):
+                response = response[0]
+
+            if self.pipeline.task == "text-generation":
+                try:
+                    from transformers.pipelines.text_generation import ReturnType
+
+                    remove_prompt = (
+                        self.pipeline._postprocess_params.get("return_type")
+                        != ReturnType.NEW_TEXT
+                    )
+                except Exception as e:
+                    logging.warning(Warnings.W017.format(e=e))
+                    remove_prompt = True
+                if remove_prompt:
+                    text = response["generated_text"][len(prompt) :]
+                else:
+                    text = response["generated_text"]
+            elif self.pipeline.task == "text2text-generation":
+                text = response["generated_text"]
+            elif self.pipeline.task == "summarization":
+                text = response["summary_text"]
+            else:
+                raise ValueError(Errors.E086.format(task=self.pipeline.task))
+
+            text_generations.append(text)
+
+        return text_generations
