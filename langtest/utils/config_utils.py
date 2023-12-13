4d0d0351ef0815ff6ac0e84cee6c82927e2fa994
Prikshit7766 <prikshitsharma8024@gmail.com>
2023-12-13 22:56:03 +0530

    loading default config

--- langtest/utils/config_utils.py
+++ langtest/utils/config_utils.py
@@ -1,18 +1,15 @@
 from pkg_resources import resource_filename
 
+
 LLM_DEFAULTS_CONFIG = {
-    "azure-openai": resource_filename("langtest", "data/config/azure_config.yml"),
-    "openai": resource_filename("langtest", "data/config/openai_config.yml"),
-    "cohere": resource_filename("langtest", "data/config/cohere_config.yml"),
-    "ai21": resource_filename("langtest", "data/config/ai21_config.yml"),
-    "huggingface-inference-api": resource_filename(
-        "langtest", "data/config/huggingface_config.yml"
-    ),
+    "azure-openai": resource_filename("langtest", "data/config/QA_summarization_azure_config.yml"),
+    "huggingface": resource_filename("langtest", "data/config/QA_summarization_huggingface_config.yml"),
+    "default": resource_filename("langtest", "data/config/QA_summarization_config.yml"),
 }
 
 DEFAULTS_CONFIG = {
-    "question-answering": LLM_DEFAULTS_CONFIG,
-    "summarization": LLM_DEFAULTS_CONFIG,
+    "question-answering": LLM_DEFAULTS_CONFIG ,
+    "summarization":  LLM_DEFAULTS_CONFIG ,
     "ideology": resource_filename("langtest", "data/config/political_config.yml"),
     "toxicity": resource_filename("langtest", "data/config/toxicity_config.yml"),
     "clinical-tests": resource_filename("langtest", "data/config/clinical_config.yml"),
@@ -20,51 +17,12 @@ DEFAULTS_CONFIG = {
     "crows-pairs": resource_filename("langtest", "data/config/crows_pairs_config.yml"),
     "stereoset": resource_filename("langtest", "data/config/stereoset_config.yml"),
     "security": resource_filename("langtest", "data/config/security_config.yml"),
-    "sensitivity-test": resource_filename(
-        "langtest", "data/config/sensitivity_config.yml"
-    ),
-    "disinformation-test": {
-        "huggingface-inference-api": resource_filename(
-            "langtest", "data/config/disinformation_huggingface_config.yml"
-        ),
-        "openai": resource_filename(
-            "langtest", "data/config/disinformation_openai_config.yml"
-        ),
-        "ai21": resource_filename(
-            "langtest", "data/config/disinformation_openai_config.yml"
-        ),
-    },
-    "factuality-test": {
-        "huggingface-inference-api": resource_filename(
-            "langtest", "data/config/factuality_huggingface_config.yml"
-        ),
-        "openai": resource_filename(
-            "langtest", "data/config/factuality_openai_config.yml"
-        ),
-        "ai21": resource_filename("langtest", "data/config/factuality_openai_config.yml"),
-    },
-    "translation": {
-        "huggingface": resource_filename(
-            "langtest", "data/config/translation_transformers_config.yml"
-        ),
-        "johnsnowlabs": resource_filename(
-            "langtest", "data/config/translation_johnsnowlabs_config.yml"
-        ),
-    },
-    "sycophancy-test": {
-        "huggingface-inference-api": resource_filename(
-            "langtest", "data/config/sycophancy_huggingface_config.yml"
-        ),
-        "openai": resource_filename(
-            "langtest", "data/config/sycophancy_openai_config.yml"
-        ),
-        "ai21": resource_filename("langtest", "data/config/sycophancy_openai_config.yml"),
-    },
-    "wino-bias": {
-        "huggingface": resource_filename(
-            "langtest", "data/config/wino_huggingface_config.yml"
-        ),
-        "openai": resource_filename("langtest", "data/config/wino_openai_config.yml"),
-        "ai21": resource_filename("langtest", "data/config/wino_ai21_config.yml"),
-    },
+    "disinformation-test": resource_filename("langtest", "disinformation_config.yml"),
+    "factuality-test": resource_filename("langtest", "factuality_config.yml"),
+    "sycophancy-test": resource_filename("langtest", "data/config/sycophancy_config.yml"),
+    "sensitivity-test": resource_filename("langtest", "data/config/sensitivity_config.yml"),
+    "translation": {"default": resource_filename("langtest", "data/config/translation_transformers_config.yml"),
+                    "johnsnowlabs": resource_filename("langtest", "data/config/translation_johnsnowlabs_config.yml"),},
+    "wino-bias": {"huggingface": resource_filename("langtest", "data/config/wino_huggingface_config.yml"),
+                    "default": resource_filename("langtest", "data/config/wino_llm_config.yml")},
 }
