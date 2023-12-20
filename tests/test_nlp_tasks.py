043b2cb3542bb3633fb2bcc3f5adb56e7219ee4e
Prikshit7766 <prikshitsharma8024@gmail.com>
2023-12-21 02:35:47 +0530

    test: added test_nlp_tasks.py

---
+++ tests/test_nlp_tasks.py
@@ -0,0 +1,187 @@
+import unittest
+import pandas as pd
+from langtest import Harness
+from langtest.utils.custom_types import Sample
+
+
+class TestNlpTask(unittest.TestCase):
+    def setUp(self):
+        super().setUp()
+        self.task_config = [
+            {
+                "task": "ner",
+                "model": {"model": "dslim/bert-base-NER", "hub": "huggingface"},
+            },
+            {
+                "task": "text-classification",
+                "model": {"model": "lvwerra/distilbert-imdb", "hub": "huggingface"},
+            },
+            {
+                "task": "question-answering",
+                "model": {"model": "t5-base", "hub": "huggingface"},
+                "data": {"data_source": "BoolQ", "split": "test-tiny"},
+                "config": {
+                    "model_parameters": {
+                        "max_tokens": 20,
+                        "task": "text2text-generation",
+                    },
+                    "evaluation": {
+                        "metric": "embedding_distance",
+                        "distance": "cosine",
+                        "threshold": 0.9,
+                    },
+                    "tests": {
+                        "defaults": {"min_pass_rate": 0.65},
+                        "robustness": {
+                            "uppercase": {"min_pass_rate": 0.75},
+                            "add_ocr_typo": {"min_pass_rate": 0.75},
+                        },
+                    },
+                },
+            },
+            {
+                "task": "summarization",
+                "model": {"model": "google/pegasus-xsum", "hub": "huggingface"},
+                "data": {"data_source": "XSum", "split": "test-tiny"},
+                "config": {
+                    "model_parameters": {"max_tokens": 64, "task": "summarization"},
+                    "tests": {
+                        "defaults": {"min_pass_rate": 0.65},
+                        "robustness": {
+                            "uppercase": {"min_pass_rate": 0.75},
+                            "add_ocr_typo": {"min_pass_rate": 0.75},
+                        },
+                    },
+                },
+            },
+            {
+                "task": {"task": "text-generation", "category": "toxicity"},
+                "model": {"model": "gpt2", "hub": "huggingface"},
+                "data": {"data_source": "Toxicity", "split": "test"},
+            },
+            {
+                "task": "translation",
+                "model": {"model": "t5-base", "hub": "huggingface"},
+                "data": {"data_source": "Translation", "split": "test"},
+            },
+            {
+                "task": {"task": "text-generation", "category": "clinical-tests"},
+                "model": {"model": "gpt2", "hub": "huggingface"},
+                "data": {"data_source": "Clinical", "split": "Medical-files"},
+            },
+            {
+                "task": {"task": "text-generation", "category": "security"},
+                "model": {"model": "gpt2", "hub": "huggingface"},
+                "data": {"data_source": "Prompt-Injection-Attack"},
+            },
+            {
+                "task": {"task": "text-generation", "category": "disinformation-test"},
+                "model": {"model": "gpt2", "hub": "huggingface"},
+                "data": {"data_source": "Narrative-Wedging"},
+            },
+            {
+                "task": {"task": "question-answering", "category": "ideology"},
+                "model": {"model": "gpt2", "hub": "huggingface"},
+            },
+            {
+                "task": {"task": "question-answering", "category": "factuality-test"},
+                "model": {"model": "t5-base", "hub": "huggingface"},
+                "data": {"data_source": "Factual-Summary-Pairs", "split": "test"},
+                "config": {
+                    "model_parameters": {
+                        "max_tokens": 20,
+                        "task": "text2text-generation",
+                    },
+                    "tests": {
+                        "defaults": {
+                            "min_pass_rate": 1.0,
+                        },
+                        "factuality": {
+                            "order_bias": {"min_pass_rate": 0.70},
+                        },
+                    },
+                },
+            },
+            {
+                "task": {"task": "question-answering", "category": "sensitivity-test"},
+                "model": {"model": "gpt2", "hub": "huggingface"},
+                "data": {"data_source": "NQ-open", "split": "test-tiny"},
+            },
+            {
+                "task": {"task": "fill-mask", "category": "wino-bias"},
+                "model": {"model": "bert-base-uncased", "hub": "huggingface"},
+                "data": {"data_source": "Wino-test", "split": "test"},
+            },
+            {
+                "task": {"task": "question-answering", "category": "legal-tests"},
+                "model": {"model": "t5-base", "hub": "huggingface"},
+                "data": {"data_source": "Legal-Support"},
+                "config": {
+                    "model_parameters": {
+                        "max_tokens": 20,
+                        "task": "text2text-generation",
+                    },
+                    "tests": {
+                        "defaults": {
+                            "min_pass_rate": 1.0,
+                        },
+                        "legal": {"legal-support": {"min_pass_rate": 0.70}},
+                    },
+                },
+            },
+            {
+                "task": {"task": "question-answering", "category": "sycophancy-test"},
+                "model": {"model": "t5-base", "hub": "huggingface"},
+                "data": {"data_source": "synthetic-math-data"},
+                "config": {
+                    "model_parameters": {
+                        "max_tokens": 20,
+                        "task": "text2text-generation",
+                    },
+                    "tests": {
+                        "defaults": {
+                            "min_pass_rate": 1.0,
+                        },
+                        "sycophancy": {"sycophancy_math": {"min_pass_rate": 0.70}},
+                    },
+                },
+            },
+            {
+                "task": {"task": "fill-mask", "category": "crows-pairs"},
+                "model": {"model": "bert-base-uncased", "hub": "huggingface"},
+                "data": {"data_source": "Crows-Pairs"},
+            },
+            {
+                "task": {"task": "question-answering", "category": "stereoset"},
+                "model": {"model": "bert-base-uncased", "hub": "huggingface"},
+                "data": {"data_source": "StereoSet"},
+            },
+        ]
+
+    def test_nlp_task(self):
+        """Test the Language Model Harness for various NLP tasks.
+
+        This test iterates over different task configurations, generates test cases,
+        runs the harness, and validates the results and reports.
+
+        Raises:
+            AssertionError: If any of the assertions fail during the test.
+        """
+        for task_parameters in self.task_config:
+            harness_instance = Harness(**task_parameters)
+            harness_instance.data = harness_instance.data[:20]
+            harness_instance.generate()
+            test_cases = harness_instance._testcases
+            assert isinstance(test_cases, list)
+            assert isinstance(test_cases[0], Sample.__constraints__)
+
+            harness_instance.run()
+            generated_results = harness_instance._generated_results
+            assert isinstance(generated_results, list)
+            assert isinstance(generated_results[0], Sample.__constraints__)
+
+            result_df_from_generated = harness_instance.generated_results()
+            result_df_from_report = harness_instance.report()
+
+            assert isinstance(result_df_from_generated, pd.DataFrame)
+            assert isinstance(result_df_from_report, pd.DataFrame)
