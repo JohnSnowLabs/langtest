1acfeb4d53bf4c61ae18ad78563325b4759a815a
Kalyan Chakravarthy <chakravarthik27@gmail.com>
2024-01-24 17:50:05 +0530

    Refactor dataset loading logic

--- langtest/langtest.py
+++ langtest/langtest.py
@@ -88,7 +88,7 @@ class Harness:
         self,
         task: Union[str, dict],
         model: Optional[Union[list, dict]] = None,
-        data: Optional[dict] = None,
+        data: Optional[Union[list, dict]] = None,
         config: Optional[Union[str, dict]] = None,
     ):
         """Initialize the Harness object.
@@ -135,26 +135,30 @@ class Harness:
         self.task = TaskManager(task)
 
         # Loading default datasets
-        if data is None and (self.task, model, hub) in self.DEFAULTS_DATASET:
-            data_path = os.path.join(
-                "data", self.DEFAULTS_DATASET[(self.task, model, hub)]
-            )
-            data = {"data_source": resource_filename("langtest", data_path)}
-            self.data = DataFactory(data, task=self.task).load()
-            if model == "textcat_imdb":
-                model = resource_filename("langtest", "data/textcat_imdb")
-            self.is_default = True
-            logging.info(Warnings.W002.format(info=(self.task, model, hub)))
-        elif data is None and self.task.category == "ideology":
-            self.data = []
-        elif data is None and (task, model, hub) not in self.DEFAULTS_DATASET.keys():
-            raise ValueError(Errors.E004)
+        if isinstance(data, list):
+            self.data = self.__multi_datasets_loading(task, hub, model, data)
+        else:
+            self.data = self.__single_dataset_loading(task, hub, model, data)
+        # if data is None and (self.task, model, hub) in self.DEFAULTS_DATASET:
+        #     data_path = os.path.join(
+        #         "data", self.DEFAULTS_DATASET[(self.task, model, hub)]
+        #     )
+        #     data = {"data_source": resource_filename("langtest", data_path)}
+        #     self.data = DataFactory(data, task=self.task).load()
+        #     if model == "textcat_imdb":
+        #         model = resource_filename("langtest", "data/textcat_imdb")
+        #     self.is_default = True
+        #     logging.info(Warnings.W002.format(info=(self.task, model, hub)))
+        # elif data is None and self.task.category == "ideology":
+        #     self.data = []
+        # elif data is None and (task, model, hub) not in self.DEFAULTS_DATASET.keys():
+        #     raise ValueError(Errors.E004)
 
-        if isinstance(data, dict):
-            if isinstance(data.get("data_source"), list):
-                self.data = data.get("data_source")
-            else:
-                self.data = DataFactory(data, task=self.task).load()
+        # if isinstance(data, dict):
+        #     if isinstance(data.get("data_source"), list):
+        #         self.data = data.get("data_source")
+        #     else:
+        #         self.data = DataFactory(data, task=self.task).load()
 
         # config loading
         if config is not None:
@@ -1304,3 +1308,37 @@ class Harness:
                 repo_id=repo_name,
                 token=token,
             )
+
+    def __multi_datasets_loading(self, task, hub, model, data):
+        loaded_data = []
+        for dataset in data:
+            processed_data = self.__single_dataset_loading(task, hub, model, dataset)
+            # loaded_data[dataset["data_source"]] = processed_data
+            loaded_data.extend(processed_data)
+
+        return loaded_data
+
+    def __single_dataset_loading(self, task, hub, model, data):
+        # Loading default datasets
+        if data is None and (self.task, model, hub) in self.DEFAULTS_DATASET:
+            data_path = os.path.join(
+                "data", self.DEFAULTS_DATASET[(self.task, model, hub)]
+            )
+            data = {"data_source": resource_filename("langtest", data_path)}
+            o_data = DataFactory(data, task=self.task).load()
+            if model == "textcat_imdb":
+                model = resource_filename("langtest", "data/textcat_imdb")
+            self.is_default = True
+            logging.info(Warnings.W002.format(info=(self.task, model, hub)))
+        elif data is None and self.task.category == "ideology":
+            self.data = []
+        elif data is None and (task, model, hub) not in self.DEFAULTS_DATASET.keys():
+            raise ValueError(Errors.E004)
+
+        if isinstance(data, dict):
+            if isinstance(data.get("data_source"), list):
+                o_data = data.get("data_source")
+            else:
+                o_data = DataFactory(data, task=self.task).load()
+
+        return o_data
