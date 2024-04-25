74589d30133e10886200aed1c8597095bb03fc95
Kalyan Chakravarthy <chakravarthik27@gmail.com>
2024-04-25 21:05:44 +0530

    Refactor Augmenter class to improve code organization and readability

--- langtest/augmentation/augmenter.py
+++ langtest/augmentation/augmenter.py
@@ -1,14 +1,23 @@
 import yaml
 
 from typing import Iterable, Union
+from langtest.datahandler.datasource import DataFactory
 from langtest.transform import TestFactory
 from langtest.tasks.task import TaskManager
 
 
 class Augmenter:
-    def __init__(
-        self, config: Union[str, dict], task: Union[str, TaskManager], columns_info=None
-    ) -> None:
+    def __init__(self, task: Union[str, TaskManager], config: Union[str, dict]) -> None:
+        """
+        Initialize the Augmenter.
+
+        Args:
+            config (Union[str, dict]): Configuration file or dictionary.
+            task (Union[str, TaskManager]): Task Manager.
+            columns_info ([type], optional): Columns information. Defaults to None.
+
+        """
+
         self.__config = config
         if isinstance(config, str):
             self.__config = self.load_config(config)
@@ -17,10 +26,17 @@ class Augmenter:
         if isinstance(task, str):
             task = TaskManager(task)
         self.__task = task
+
+        # Test Factory and Data Factory
         self.__testfactory = TestFactory
-        self.features = columns_info.get("features", [])
-        self.target = columns_info.get("target", None)
+        self.__datafactory = DataFactory
+
         self.__testfactory.is_augment = True
+
+        # parameters
+        self.__max_proportion = self.__tests.get("defaults", 0.6).get(
+            "max_proportion", 0.6
+        )
 
     def load_config(self, config: str) -> dict:
         """
@@ -29,31 +45,39 @@ class Augmenter:
         with open(config, "r") as f:
             return yaml.safe_load(f)
 
-    def augment(self, data: Iterable) -> str:
+    def augment(self, data: Union[str, Iterable]) -> str:
         """
         Augment the content.
         """
+        # load the data
+        if isinstance(data, dict):
+            self.__datafactory = self.__datafactory(file_path=data, task=self.__task)
+            data = self.__datafactory.load()
         # prepare the data for augmentation
         categories = list(self.__tests.keys())
-
-        # iterate over the categories
-        test_types = self.__tests
 
         testcases = []
         for category in categories:
             if category not in ["robustness", "bias"]:
                 continue
-            R_data = [
-                self.__task.create_sample(
-                    row_data=sample,
-                    feature_column=self.features,
-                    target_column=self.target,
-                )
-                for sample in data
-            ]
-            test_cases = self.__testfactory.transform(self.__task, R_data, test_types)
+
+            test_cases = self.__testfactory.transform(self.__task, data, self.__tests)
             testcases.extend(test_cases)
-        return testcases
+
+        self.__augmented_data = testcases
+
+        return self
+
+    def prepare_hash_map(self, data: Union[str, Iterable]) -> str:
+        hashmap = {index: sample for index, sample in enumerate(data)}
+
+        return hashmap
+
+    def save(self, file_path: str):
+        """
+        Save the augmented data.
+        """
+        self.__datafactory.export(data=self.__augmented_data, output_path=file_path)
 
     def __or__(self, other: Iterable):
         results = self.augment(other)
