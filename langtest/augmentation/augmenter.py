55d17e1d9bf319314812193c2d541104d35f9e0f
Kalyan Chakravarthy <chakravarthik27@gmail.com>
2024-08-16 14:30:50 +0530

    chore: Refactor DataAugmenter to improve template generation and proportion handling

--- langtest/augmentation/augmenter.py
+++ langtest/augmentation/augmenter.py
@@ -26,7 +26,7 @@ class DataAugmenter:
         if isinstance(config, str):
             self.__config = self.load_config(config)
 
-        self.__tests: dict = self.__config.get("tests", [])
+        self.__tests: Dict[str, Dict[str, dict]] = self.__config.get("tests", [])
         if isinstance(task, str):
             if task in ["ner", "text-classification", "question-answering"]:
                 task = TaskManager(task)
@@ -275,6 +275,9 @@ class DataAugmenter:
                     }
                 )
         df = pd.concat([df, pd.DataFrame(temp_data)], ignore_index=True)
+
+        # Convert 'proportion' column to float
+        df["proportion"] = pd.to_numeric(df["proportion"], errors="coerce")
 
         # normalize the proportion and round it to 2 decimal places
         df["normalized_proportion"] = df["proportion"] / df["proportion"].sum()
