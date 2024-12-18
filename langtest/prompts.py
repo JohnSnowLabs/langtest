596d9875e74ce49d13c551966a923ae8978d64ab
Kalyan Chakravarthy <chakravarthik27@gmail.com>
2024-12-18 22:03:17 +0530

    feat: update prompt handling to support 'instruct' and 'completion' types in PromptConfig

--- langtest/prompts.py
+++ langtest/prompts.py
@@ -131,7 +131,7 @@ class PromptConfig(BaseModel):
 
     def prompt_style(self):
         """Generate a prompt based on the prompt type."""
-        if self.prompt_type == "chat":
+        if self.prompt_type in ["chat", "instruct"]:
             from langchain.prompts import (
                 ChatPromptTemplate,
                 FewShotChatMessagePromptTemplate,
@@ -154,7 +154,7 @@ class PromptConfig(BaseModel):
             )
             return final_prompt
 
-        elif self.prompt_type == "instruct":
+        elif self.prompt_type == "completion":
             from langchain.prompts import FewShotPromptTemplate, PromptTemplate
 
             template = "".join(v for _, v in self.get_template)
